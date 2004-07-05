292307253153ae8aa6b5fe47fe93614c879b09ce
Emil Brisk <emil.brink@interactiveinstitute.se>
2004-07-05 12:19:34 +0000

    Added comments
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@266 49c17534-1d02-dc11-b283-0030488c597c

--- purple/dynlib.h
+++ purple/dynlib.h
@@ -19,6 +19,11 @@ typedef HMODULE	DynLib;
 */
 void	dynlib_path_set(const char *path);
 
+/* Attempt to load the named dynamic library. Test return value with DYNLIB_VALID(). */
 DynLib	dynlib_load(const char *name);
+
+/* Resolve <symbol> against loaded <lib>. Returns NULL on failure. */ 
 void *	dynlib_resolve(const DynLib lib, const char *symbol);
+
+/* Unload a previously loaded library. Don't access symbols after this, duh. */
 void	dynlib_unload(DynLib lib);
