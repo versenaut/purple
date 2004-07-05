bcc4b5eb40e347dbe0731e466770c0b527c41bcd
Emil Brisk <emil.brink@interactiveinstitute.se>
2004-07-05 11:46:02 +0000

    Nicer prototype
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@261 49c17534-1d02-dc11-b283-0030488c597c

--- purple/cron.h
+++ purple/cron.h
@@ -14,7 +14,7 @@ typedef enum
 extern void		cron_init(void);
 
 extern unsigned int	cron_add(CronTimeType type, double seconds, int (*handler)(void *data), void *data);
-extern void		cron_set(unsigned int id, double seconds, int (*handler)(void *), void *data);
+extern void		cron_set(unsigned int id, double seconds, int (*handler)(void *data), void *data);
 extern void		cron_remove(unsigned int handle);
 
 extern void		cron_update(void);
