faeefeb65dc30fe78033a3408dba224dda9e9a0e
Emil Brisk <emil.brink@interactiveinstitute.se>
2004-07-05 13:04:29 +0000

    Better comments
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@274 49c17534-1d02-dc11-b283-0030488c597c

--- purple/dynstr.h
+++ purple/dynstr.h
@@ -9,21 +9,33 @@
 
 typedef struct DynStr	DynStr;
 
+/* Create a new, empty string. */
 extern DynStr *	dynstr_new(const char *text);
+
+/* Set the string <str> to <text>. */
 extern void	dynstr_assign(DynStr *str, const char *text);
 
+/* Format content into <str>. */
 extern void	dynstr_printf(DynStr *str, const char *fmt, ...);
 
+/* Append <text> to <str>. */
 extern void	dynstr_append(DynStr *str, const char *text);
+
+/* Append the single character <c> to <str>. */
 extern void	dynstr_append_c(DynStr *str, char c);
+
+/* Append formatted text to <str>. */
 extern void	dynstr_append_printf(DynStr *str, const char *fmt, ...);
 
+/* Return the string <str>'s character buffer. Read only, please. */
 extern const char * dynstr_string(const DynStr *str);
+
+/* Return the length of <str>. O(1). */
 extern size_t	dynstr_length(const DynStr *str);
 
-/* Destroy a dynamic string. If <destroy_buffer> is non-zero, it will
+/* Destroy string <str>. If <destroy_buffer> is non-zero, it will
  * also destroy the actual string buffer. If not, it is returned.
 */
-extern char *	dynstr_destroy(DynStr *ds, int destroy_buffer);
+extern char *	dynstr_destroy(DynStr *str, int destroy_buffer);
 
 #endif		/* DYNSTR_H */
