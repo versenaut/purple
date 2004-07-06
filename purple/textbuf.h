fc7909985d01ff1d85ff636a7b9853f101ed6dd0
Emil Brisk <emil.brink@interactiveinstitute.se>
2004-07-06 10:28:54 +0000

    Added friendly interface comments
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@303 49c17534-1d02-dc11-b283-0030488c597c

--- purple/textbuf.h
+++ purple/textbuf.h
@@ -5,12 +5,27 @@
 
 typedef struct TextBuf	TextBuf;
 
+/* Create a fresh empty text buffer. Preallocate <inital_size> bytes of space,
+ * if non-zero.
+*/
 extern TextBuf *	textbuf_new(size_t initial_size);
 
+/* Insert <text>, beginning at <offset> in the given buffer. Any trailing text will
+ * be pushed towards the end of the buffer; no characters are lost. If <offset> is
+ * beyond end of buffer, insert will become an append. Use offset=0 to prepend.
+*/
 extern void		textbuf_insert(TextBuf *tb, size_t offset, const char *text);
+
+/* Delete region [offset, offset+length-1]. */
 extern void		textbuf_delete(TextBuf *tb, size_t offset, size_t length);
 
+/* Return the text, as an ordinary C string with NUL termination and everything.
+ * Depending on the underlying implementation, this is potentially expensive.
+*/
 extern const char *	textbuf_text(TextBuf *tb);
+
+/* Return the length of the text buffer, in characters. */
 extern size_t		textbuf_length(const TextBuf *tb);
 
+/* Destroy a text buffer, losing the text of course. */
 extern void		textbuf_destroy(TextBuf *tb);
