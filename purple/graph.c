e3b949161e9094333607130f8800aafb0a352d88
Emil Brisk <emil.brink@interactiveinstitute.se>
2004-07-27 10:12:23 +0000

    Updated method interface for graph create
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@370 49c17534-1d02-dc11-b283-0030488c597c

--- purple/graph.c
+++ purple/graph.c
@@ -2,26 +2,42 @@
  * 
 */
 
+#include <stdio.h>
 #include <string.h>
 
 #include "verse.h"
 
+#include "hash.h"
 #include "log.h"
+#include "textbuf.h"
+
+#include "client.h"
 
 #include "graph.h"
 
 /* ----------------------------------------------------------------------------------------- */
 
-static const VNOParamType	create_type[]   = { VN_O_METHOD_PTYPE_STRING },
+typedef struct
+{
+	uint32	id;
+	char	name[32];
+
+	VNodeID	node;
+	uint16	buffer;
+} Graph;
+
+static const VNOParamType	create_type[]   = { VN_O_METHOD_PTYPE_NODE, VN_O_METHOD_PTYPE_LAYER,
+							VN_O_METHOD_PTYPE_STRING },
 				rename_type[]   = { VN_O_METHOD_PTYPE_UINT32 },
 				destroy_type[]  = { VN_O_METHOD_PTYPE_UINT32 };
-static char			*create_name[]  = { "name" },
+static char			*create_name[]  = { "node", "buffer", "name" },
 				*rename_name[]  = { "graph" },
 				*destroy_name[] = { "graph" };
 
 static struct
 {
 	uint8	mid_create, mid_rename, mid_destroy;
+	Hash	*graphs;
 } graph_info;
 
 /* ----------------------------------------------------------------------------------------- */
@@ -47,20 +63,27 @@ void graph_method_receive_create(uint8 id, const char *name)
 	else if(strcmp(name, "graph_destroy") == 0)
 		graph_info.mid_destroy = id;
 	else
-		LOG_WARN(("Received unknown graph method creation, %s()", name));
+		LOG_WARN(("Received unknown (non graph-related?) method creation, %s()", name));
+}
+
+/* ----------------------------------------------------------------------------------------- */
+
+static void graph_create(VNodeID node_id, uint16 buffer_id, const char *name)
+{
+	printf("Create graph named '%s' in node %u, buffer %u\n", name, node_id, buffer_id);
 }
 
 /* ----------------------------------------------------------------------------------------- */
 
 void graph_method_receive_call(uint8 id, const void *param)
 {
-	VNOParam	arg;
+	VNOParam	arg[sizeof create_type / sizeof *create_type];
 
 	if(id == graph_info.mid_create)
 	{
 		printf("create, create\n");
-		if(verse_method_call_unpack(param, sizeof create_type / sizeof *create_type, &arg, create_type))
-			printf(" '%s'\n", arg.vstring);
+		if(verse_method_call_unpack(param, sizeof create_type / sizeof *create_type, create_type, arg))
+			graph_create(arg[0].vnode, arg[1].vlayer, arg[2].vstring);
 	}
 	else if(id == graph_info.mid_rename)
 		;
