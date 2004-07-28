540fde37471f213b57010a5f78159d71d29276c6
Emil Brisk <emil.brink@interactiveinstitute.se>
2004-07-28 08:19:55 +0000

    Fleshed out graph data structure; added (temp) create call
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@376 49c17534-1d02-dc11-b283-0030488c597c

--- purple/graph.c
+++ purple/graph.c
@@ -8,7 +8,10 @@
 #include "verse.h"
 
 #include "hash.h"
+#include "list.h"
 #include "log.h"
+#include "mem.h"
+#include "strutil.h"
 #include "textbuf.h"
 
 #include "client.h"
@@ -37,10 +40,18 @@ static char			*create_name[]  = { "node", "buffer", "name" },
 static struct
 {
 	uint8	mid_create, mid_rename, mid_destroy;
-	Hash	*graphs;
-} graph_info;
+	uint32	next_id;
+	List	*free_ids;
+	Hash	*graphs_id, *graphs_name;	/* Graphs hashed on ID and name, respectively. */
+} graph_info = { 0 };
 
 /* ----------------------------------------------------------------------------------------- */
+
+void graph_init(void)
+{
+	graph_info.next_id  = 0;
+	graph_info.free_ids = NULL;
+}
 
 void graph_method_send_creates(uint32 avatar, uint8 group_id)
 {
@@ -68,12 +79,71 @@ void graph_method_receive_create(uint8 id, const char *name)
 
 /* ----------------------------------------------------------------------------------------- */
 
-static void graph_create(VNodeID node_id, uint16 buffer_id, const char *name)
+static unsigned int graph_direct_hash(const void *a)
 {
+	return ((const Graph *) a)->id;
+}
+
+static int graph_direct_compare(const void *a, const void *b)
+{
+	return strcmp(((const Graph *) a)->name, ((const Graph *) b)->name) == 0;
+}
+
+static Graph * graph_create(VNodeID node_id, uint16 buffer_id, const char *name)
+{
+	Graph	*g;
+
 	printf("Create graph named '%s' in node %u, buffer %u\n", name, node_id, buffer_id);
+	/* Make sure name is unique. */
+	if(hash_lookup(graph_info.graphs_name, name) != NULL)
+		return NULL;	/* It wasn't. */
+
+	if(graph_info.graphs_id == NULL)
+		graph_info.graphs_id = hash_new(graph_direct_hash, graph_direct_compare);
+	if(graph_info.graphs_name == NULL)
+		graph_info.graphs_name = hash_new_string();
+
+	g = mem_alloc(sizeof *g);
+	if(graph_info.free_ids != NULL)
+	{
+		g->id = (uint32) list_data(graph_info.free_ids);
+		graph_info.free_ids = list_tail(graph_info.free_ids);
+	}
+	else
+		g->id = graph_info.next_id++;
+	stu_strncpy(g->name, sizeof g->name, name);
+	g->node   = node_id;
+	g->buffer = buffer_id;
+
+	printf("<graph name=\"%s\" id=\"%u\">\n"
+	       " <at>\n"
+	       "  <node>%u</node>\n"
+	       "  <buffer>%u</buffer>\n"
+	       " </at>\n"
+	       "</graph>\n", g->name, g->id, node_id, buffer_id);
+
+	return g;
 }
 
 /* ----------------------------------------------------------------------------------------- */
+
+void graph_method_send_call_create(VNodeID node, VLayerID buffer, const char *name)
+{
+	VNOParam	param[sizeof create_type / sizeof *create_type];
+	void		*pack;
+
+	param[0].vnode   = node;
+	param[1].vlayer  = buffer;
+	param[2].vstring = name;
+	if((pack = verse_method_call_pack(sizeof create_type / sizeof *create_type, create_type, param)) != NULL)
+	{
+		verse_send_o_method_call(client_info.avatar, client_info.gid_control, graph_info.mid_create, 0, pack);
+	}
+}
+
+void graph_method_send_call_destroy(uint32 id)
+{
+}
 
 void graph_method_receive_call(uint8 id, const void *param)
 {
@@ -81,7 +151,6 @@ void graph_method_receive_call(uint8 id, const void *param)
 
 	if(id == graph_info.mid_create)
 	{
-		printf("create, create\n");
 		if(verse_method_call_unpack(param, sizeof create_type / sizeof *create_type, create_type, arg))
 			graph_create(arg[0].vnode, arg[1].vlayer, arg[2].vstring);
 	}
