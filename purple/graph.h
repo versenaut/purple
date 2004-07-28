f0e41f0df9207e0effa4642eac13aa29f3827613
Emil Brisk <emil.brink@interactiveinstitute.se>
2004-07-28 08:20:20 +0000

    Added init() for statics; added temporary create/delete calls
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@377 49c17534-1d02-dc11-b283-0030488c597c

--- purple/graph.h
+++ purple/graph.h
@@ -2,6 +2,12 @@
  * 
 */
 
+extern void	graph_init(void);
+
 extern void	graph_method_send_creates(uint32 avatar, uint8 group_id);
+
+extern void	graph_method_send_call_create(VNodeID node, VLayerID buffer, const char *name);
+extern void	graph_method_send_call_destroy(uint32 id);
+
 extern void	graph_method_receive_create(uint8 id, const char *name);
 extern void	graph_method_receive_call(uint8 id, const void *param);
