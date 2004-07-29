c292c5eae6bf9634811b67db9357316d863b3a55
Emil Brisk <emil.brink@interactiveinstitute.se>
2004-07-29 10:27:38 +0000

    Use unsigned int; reindent
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@402 49c17534-1d02-dc11-b283-0030488c597c

--- purple/idset.h
+++ purple/idset.h
@@ -1,18 +1,20 @@
 /*
  * IdSet, a simple data structure for managing a set of objects, each of which is
  * given a numerical ID. Objects can be inserted and removed, with IDs re-used from
- * removed objects.
+ * removed objects. IDs of inserted objects never change.
 */
 
 typedef struct IdSet	IdSet;
 
-extern IdSet *	idset_new(void);
+extern IdSet *		idset_new(void);
 
-extern uint32	idset_insert(IdSet *is, void *object);
-extern void	idset_remove(IdSet *is, uint32 id);
+extern unsigned int	idset_insert(IdSet *is, void *object);
+extern void		idset_remove(IdSet *is, unsigned int id);
 
-extern size_t	idset_size(const IdSet *is);
+extern size_t		idset_size(const IdSet *is);
 
-extern void *	idset_lookup(const IdSet *is, uint32 id);
+extern void *		idset_lookup(const IdSet *is, unsigned int id);
 
-extern void	idset_destroy(IdSet *is);
+extern void *		idset_foreach_next(const IdSet *is, unsigned int *id);
+
+extern void		idset_destroy(IdSet *is);
