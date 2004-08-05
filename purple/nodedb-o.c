6a416454da25cff6d89ef2dc244c796e0b153ce9
Emil Brisk <emil.brink@interactiveinstitute.se>
2004-08-05 15:16:25 +0000

    Added light tracking
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@495 49c17534-1d02-dc11-b283-0030488c597c

--- purple/nodedb-o.c
+++ purple/nodedb-o.c
@@ -24,8 +24,22 @@ void nodedb_o_init(NodeObject *n)
 
 /* ----------------------------------------------------------------------------------------- */
 
+static void cb_o_light_set(void *user, VNodeID node_id, real64 r, real64 g, real64 b)
+{
+	NodeObject	*n;
+
+	if((n = nodedb_lookup_object(node_id)) != NULL)
+	{
+		n->light[0] = r;
+		n->light[1] = g;
+		n->light[2] = b;
+		NOTIFY(n, DATA);
+	}
+}
+
 /* ----------------------------------------------------------------------------------------- */
 
 void nodedb_o_register_callbacks(void)
 {
+	verse_callback_set(verse_send_o_light_set, cb_o_light_set, NULL);
 }
