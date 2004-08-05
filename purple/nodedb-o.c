92ca20b908f340389223c0163e5ec7b9677789d2
Emil Brisk <emil.brink@interactiveinstitute.se>
2004-08-05 15:28:17 +0000

    Link setting implemented
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@498 49c17534-1d02-dc11-b283-0030488c597c

--- purple/nodedb-o.c
+++ purple/nodedb-o.c
@@ -19,10 +19,29 @@
 void nodedb_o_init(NodeObject *n)
 {
 	n->light[0] = n->light[1] = n->light[2] = 0.0;
+	n->links = dynarr_new(sizeof (NdbOLink), 1);
 	n->method_groups = dynarr_new(sizeof (NdbOMethodGroup), 1);
 }
 
 /* ----------------------------------------------------------------------------------------- */
+
+static void cb_o_link_set(void *user, VNodeID node_id, uint16 link_id, uint32 link, const char *name, uint32 target_id)
+{
+	NodeObject	*n;
+
+	if((n = nodedb_lookup_object(node_id)) != NULL)
+	{
+		NdbOLink	*l;
+
+		if((l = dynarr_set(n->links, link_id, NULL)) != NULL)
+		{
+			l->id = link_id;
+			l->link = link;
+			stu_strncpy(l->name, sizeof l->name, name);
+			l->target = target_id;
+		}
+	}
+}
 
 static void cb_o_light_set(void *user, VNodeID node_id, real64 r, real64 g, real64 b)
 {
@@ -41,5 +60,6 @@ static void cb_o_light_set(void *user, VNodeID node_id, real64 r, real64 g, real
 
 void nodedb_o_register_callbacks(void)
 {
-	verse_callback_set(verse_send_o_light_set, cb_o_light_set, NULL);
+	verse_callback_set(verse_send_o_link_set,	cb_o_link_set, NULL);
+	verse_callback_set(verse_send_o_light_set,	cb_o_light_set, NULL);
 }
