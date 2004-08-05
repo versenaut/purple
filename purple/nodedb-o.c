4c10a9064e02d7507e09c04a95ee0b03a6320012
Emil Brisk <emil.brink@interactiveinstitute.se>
2004-08-05 15:34:35 +0000

    Added (lazy) method group creation
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@500 49c17534-1d02-dc11-b283-0030488c597c

--- purple/nodedb-o.c
+++ purple/nodedb-o.c
@@ -20,7 +20,7 @@ void nodedb_o_init(NodeObject *n)
 {
 	n->light[0] = n->light[1] = n->light[2] = 0.0;
 	n->links = dynarr_new(sizeof (NdbOLink), 1);
-	n->method_groups = dynarr_new(sizeof (NdbOMethodGroup), 1);
+	n->method_groups = NULL;
 }
 
 /* ----------------------------------------------------------------------------------------- */
@@ -57,10 +57,31 @@ static void cb_o_light_set(void *user, VNodeID node_id, real64 r, real64 g, real
 	}
 }
 
+
+static void cb_o_method_group_create(void *user, VNodeID node_id, uint16 group_id, const char *name)
+{
+	NodeObject	*n;
+
+	if((n = nodedb_lookup_object(node_id)) != NULL)
+	{
+		NdbOMethodGroup	*g;
+
+		if(n->method_groups == NULL)
+			n->method_groups = dynarr_new(sizeof (NdbOMethodGroup), 1);
+		if((g = dynarr_set(n->method_groups, group_id, NULL)) != NULL)
+		{
+			g->id = group_id;
+			stu_strncpy(g->name, sizeof name, name);
+			g->methods = NULL;
+		}
+	}
+}
+
 /* ----------------------------------------------------------------------------------------- */
 
 void nodedb_o_register_callbacks(void)
 {
-	verse_callback_set(verse_send_o_link_set,	cb_o_link_set, NULL);
-	verse_callback_set(verse_send_o_light_set,	cb_o_light_set, NULL);
+	verse_callback_set(verse_send_o_link_set,		cb_o_link_set, NULL);
+	verse_callback_set(verse_send_o_light_set,		cb_o_light_set, NULL);
+	verse_callback_set(verse_send_o_method_group_create,	cb_o_method_group_create, NULL);
 }
