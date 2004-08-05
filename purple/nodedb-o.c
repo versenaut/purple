de3f03895647094db7b1c08bb011794745acb2b3
Emil Brisk <emil.brink@interactiveinstitute.se>
2004-08-05 15:35:04 +0000

    Lazied down links
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@501 49c17534-1d02-dc11-b283-0030488c597c

--- purple/nodedb-o.c
+++ purple/nodedb-o.c
@@ -19,7 +19,7 @@
 void nodedb_o_init(NodeObject *n)
 {
 	n->light[0] = n->light[1] = n->light[2] = 0.0;
-	n->links = dynarr_new(sizeof (NdbOLink), 1);
+	n->links = NULL;
 	n->method_groups = NULL;
 }
 
@@ -33,6 +33,8 @@ static void cb_o_link_set(void *user, VNodeID node_id, uint16 link_id, uint32 li
 	{
 		NdbOLink	*l;
 
+		if(n->links == NULL)
+			n->links = dynarr_new(sizeof (NdbOLink), 1);
 		if((l = dynarr_set(n->links, link_id, NULL)) != NULL)
 		{
 			l->id = link_id;
