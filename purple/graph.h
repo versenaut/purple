f6cfa835d2ef5cf03ba6e358f51d6abbbc7bd6a0
Emil Brisk <emil.brink@interactiveinstitute.se>
2004-08-12 14:56:57 +0000

    Renamed method creation handler; commented
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@555 49c17534-1d02-dc11-b283-0030488c597c

--- purple/graph.h
+++ purple/graph.h
@@ -1,11 +1,16 @@
 /*
- * 
+ * Graph editing functionality.
 */
 
+/* Initialize graph module. Must be called before module is used. */
 extern void	graph_init(void);
 
+/* Send Verse method creation commands for the group editing API on the given avatar. */
 extern void	graph_method_send_creates(uint32 avatar, uint8 group_id);
 
+/* These are used for local, "console", testing. In an actual production version of Purple,
+ * they might be exluded since the calls are expected to come in from dedicated UI clients.
+*/
 extern void	graph_method_send_call_create(VNodeID node, VLayerID buffer, const char *name);
 extern void	graph_method_send_call_destroy(uint32 id);
 extern void	graph_method_send_call_mod_create(uint32 graph_id, uint32 plugin_id);
@@ -13,5 +18,8 @@ extern void	graph_method_send_call_mod_destroy(uint32 graph_id, uint32 module_id
 extern void	graph_method_send_call_mod_input_set(uint32 graph_id, uint32 mod_id, uint32 index, const PInputValue *value);
 extern void	graph_method_send_call_mod_input_clear(uint32 graph_id, uint32 mod_id, uint32 input);
 
-extern void	graph_method_receive_create(NodeObject *node);
+/* Check if the graph editing API has been created yet. Called from nodedb notification callback. */
+extern void	graph_method_check_created(NodeObject *node);
+
+/* A method call was received, check if it's one of the graph editing calls, and if so take action. */
 extern void	graph_method_receive_call(uint8 id, const void *param);
