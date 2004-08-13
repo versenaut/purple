1cf4f9b15b8c72325239096877dd88658ae9be06
Emil Brisk <emil.brink@interactiveinstitute.se>
2004-08-13 12:53:31 +0000

    Tentative implementation
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@573 49c17534-1d02-dc11-b283-0030488c597c

--- purple/api-input.c
+++ purple/api-input.c
@@ -14,7 +14,26 @@
 
 real32 p_input_real32(PPInput input)
 {
+	const PInputValue	*v = input;
+
 	if(input == NULL)
-		return;
+		return 0.0f;
+	switch(v->type)
+	{
+	case P_INPUT_BOOLEAN:
+		return v->v.vboolean;
+	case P_INPUT_INT32:
+		return v->v.vint32;
+	case P_INPUT_UINT32:
+		return v->v.vuint32;
+	case P_INPUT_REAL32:
+		return v->v.vreal32;
+	case P_INPUT_REAL64:
+		return v->v.vreal64;
+	case P_INPUT_STRING:
+		return (real32) strtod(v->v.vstring, NULL);
+	default:
+		LOG_WARN(("Can't convert value of type %d to real32", v->type));
+	}
 	return 0.0f;
 }
