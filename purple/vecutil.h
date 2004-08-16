7fadc6c886181938915814e233b717b37b6e580d
Emil Brisk <emil.brink@interactiveinstitute.se>
2004-08-16 10:46:59 +0000

    Vector utilites
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@580 49c17534-1d02-dc11-b283-0030488c597c

---
+++ purple/vecutil.h
@@ -0,0 +1,12 @@
+/*
+ * Some very basic vector math utilities. Needed in input processing,
+ * but might also be useful to export to plug-in code at some point.
+*/
+
+extern real32	vec_real32_vec2_magnitude(const real32 *vec);
+extern real32	vec_real32_vec3_magnitude(const real32 *vec);
+extern real32	vec_real32_vec4_magnitude(const real32 *vec);
+
+extern real64	vec_real64_vec2_magnitude(const real64 *vec);
+extern real64	vec_real64_vec3_magnitude(const real64 *vec);
+extern real64	vec_real64_vec4_magnitude(const real64 *vec);
