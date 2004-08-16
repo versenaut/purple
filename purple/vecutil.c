1d2604afa55b11bb93f80d301b6fc7472d3f356d
Emil Brisk <emil.brink@interactiveinstitute.se>
2004-08-16 10:57:12 +0000

    Initial implementation, 2,3,4-D vector magnitudes
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@583 49c17534-1d02-dc11-b283-0030488c597c

---
+++ purple/vecutil.c
@@ -0,0 +1,38 @@
+/*
+ *
+*/
+
+#include <math.h>
+
+#include "verse.h"
+
+real32 vec_real32_vec2_magnitude(const real32 *vec)
+{
+	return sqrtf(vec[0] * vec[0] + vec[1] * vec[1]);
+}
+
+real32 vec_real32_vec3_magnitude(const real32 *vec)
+{
+	return sqrtf(vec[0] * vec[0] + vec[1] * vec[1] + vec[2] * vec[2]);
+}
+
+real32 vec_real32_vec4_magnitude(const real32 *vec)
+{
+	return sqrtf(vec[0] * vec[0] + vec[1] * vec[1] + vec[2] * vec[2] + vec[3] * vec[3]);
+}
+
+
+real64 vec_real64_vec2_magnitude(const real64 *vec)
+{
+	return sqrt(vec[0] * vec[0] + vec[1] * vec[1]);
+}
+
+real64 vec_real64_vec3_magnitude(const real64 *vec)
+{
+	return sqrt(vec[0] * vec[0] + vec[1] * vec[1] + vec[2] * vec[2]);
+}
+
+real64 vec_real64_vec4_magnitude(const real64 *vec)
+{
+	return sqrt(vec[0] * vec[0] + vec[1] * vec[1] + vec[2] * vec[2] + vec[3] * vec[3]);
+}
