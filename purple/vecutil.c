38c2500072054f818531fb5f806eba8bc8c67251
Emil Brisk <emil.brink@interactiveinstitute.se>
2004-08-17 10:55:20 +0000

    Separate 32- and 64-bit functions with pretty bar
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@589 49c17534-1d02-dc11-b283-0030488c597c

--- purple/vecutil.c
+++ purple/vecutil.c
@@ -21,6 +21,7 @@ real32 vec_real32_vec4_magnitude(const real32 *vec)
 	return sqrtf(vec[0] * vec[0] + vec[1] * vec[1] + vec[2] * vec[2] + vec[3] * vec[3]);
 }
 
+/* ----------------------------------------------------------------------------------------- */
 
 real64 vec_real64_vec2_magnitude(const real64 *vec)
 {
