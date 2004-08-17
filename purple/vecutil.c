bf7bdc08df6b16a1e15e8f539da8d4a2ef2f2175
Emil Brisk <emil.brink@interactiveinstitute.se>
2004-08-17 11:15:11 +0000

    Added (placeholder) matrix determinant functions
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@590 49c17534-1d02-dc11-b283-0030488c597c

--- purple/vecutil.c
+++ purple/vecutil.c
@@ -21,6 +21,11 @@ real32 vec_real32_vec4_magnitude(const real32 *vec)
 	return sqrtf(vec[0] * vec[0] + vec[1] * vec[1] + vec[2] * vec[2] + vec[3] * vec[3]);
 }
 
+real32 vec_real32_mat16_determinant(const real32 *mat)
+{
+	return mat[0];
+}
+
 /* ----------------------------------------------------------------------------------------- */
 
 real64 vec_real64_vec2_magnitude(const real64 *vec)
@@ -36,4 +41,9 @@ real64 vec_real64_vec3_magnitude(const real64 *vec)
 real64 vec_real64_vec4_magnitude(const real64 *vec)
 {
 	return sqrt(vec[0] * vec[0] + vec[1] * vec[1] + vec[2] * vec[2] + vec[3] * vec[3]);
+}
+
+real64 vec_real64_mat16_determinant(const real64 *mat)
+{
+	return mat[0];
 }
--- purple/vecutil.h
+++ purple/vecutil.h
@@ -7,6 +7,10 @@ extern real32	vec_real32_vec2_magnitude(const real32 *vec);
 extern real32	vec_real32_vec3_magnitude(const real32 *vec);
 extern real32	vec_real32_vec4_magnitude(const real32 *vec);
 
+extern real32	vec_real32_mat16_determinant(const real32 *mat);
+
 extern real64	vec_real64_vec2_magnitude(const real64 *vec);
 extern real64	vec_real64_vec3_magnitude(const real64 *vec);
 extern real64	vec_real64_vec4_magnitude(const real64 *vec);
+
+extern real64	vec_real64_mat16_determinant(const real64 *mat);
