4af097da7cca81848ade50416f9b187d447a56df
Emil Brisk <emil.brink@interactiveinstitute.se>
2004-08-24 07:54:32 +0000

    Lookup by name; destroy notification
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@624 49c17534-1d02-dc11-b283-0030488c597c

--- purple/nodedb.h
+++ purple/nodedb.h
@@ -30,13 +30,14 @@ typedef struct
 extern void		nodedb_register_callbacks(VNodeID avatar, uint32 mask);
 
 extern Node *		nodedb_lookup(VNodeID node_id);
+extern Node *		nodedb_lookup_by_name(const char *name);
 extern NodeObject *	nodedb_lookup_object(VNodeID node_id);	/* Convenient. */
 extern NodeText *	nodedb_lookup_text(VNodeID node_id);
 
 extern Node *		nodedb_lookup_mine_by_name(const char *name);
 
 typedef enum { NODEDB_OWNERSHIP_ALL, NODEDB_OWNERSHIP_MINE, NODEDB_OWNERSHIP_OTHERS } NodeOwnership;
-typedef enum { NODEDB_NOTIFY_CREATE, NODEDB_NOTIFY_STRUCTURE, NODEDB_NOTIFY_DATA, NODEDB_NOTIFY_DELETE } NodeNotifyEvent;
+typedef enum { NODEDB_NOTIFY_CREATE, NODEDB_NOTIFY_STRUCTURE, NODEDB_NOTIFY_DATA, NODEDB_NOTIFY_DESTROY } NodeNotifyEvent;
 
 /* Add a callback that will be called when a change occurs in a node. */
 extern void		nodedb_notify_add(NodeOwnership whose, void (*notify)(Node *node, NodeNotifyEvent ev));
