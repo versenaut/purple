07c652137033b95d98a43f2278f166650a901524
Emil Brisk <emil.brink@interactiveinstitute.se>
2004-08-24 10:07:46 +0000

    Added node notification implementation
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@629 49c17534-1d02-dc11-b283-0030488c597c

--- purple/nodedb-internal.h
+++ purple/nodedb-internal.h
@@ -3,6 +3,9 @@
  * into several C files.
 */
 
-#define	NOTIFY(n, e)	nodedb_internal_notify_mine_check((Node *) (n), NODEDB_NOTIFY_ ## e);
+#define	NOTIFY(n, e)	do { \
+				nodedb_internal_notify_mine_check((Node *) (n), NODEDB_NOTIFY_ ## e); \
+				nodedb_internal_notify_node_check((Node *) (n), NODEDB_NOTIFY_ ## e); \
+			} while(0)
 
 extern void	nodedb_internal_notify_mine_check(Node *n, NodeNotifyEvent ev);
