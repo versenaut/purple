772faf18336850925ddc5066250e69058b225de0
Emil Brisk <emil.brink@interactiveinstitute.se>
2004-08-24 13:44:49 +0000

    Plug-in template
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@646 49c17534-1d02-dc11-b283-0030488c597c

---
+++ purple/plugins/template.c
@@ -0,0 +1,38 @@
+/*
+ * Purple plug-in template. Does nothing, in an informative way.
+*/
+
+#include "purple.h"
+
+typedef struct
+{
+	int	value;	/* Just to prevent an empty struct. */
+} State;
+
+static void compute(PPInput *input, PPOutput output, void *state)
+{
+}
+
+static void ctor(void *state_typeless)
+{
+	State	*state = state_typeless;
+}
+
+static void dtor(void *state_typeless)
+{
+	State	*state = state_typeless;
+}
+
+void init(void)
+{
+	p_init_create("template");
+	/* Initialize inputs. */
+	p_init_input(0, P_INPUT_REAL32, "x", P_INPUT_REQUIRED, P_INPUT_DONE);
+	/* Set meta information. */
+	p_init_meta("desc/purpose", "Shows how to use Purple API");
+	p_init_meta("author", "Randomm Developer");
+	/* Register state structure size, and constructor/destructor functions. */
+	p_init_state(sizeof (State), ctor, dtor);
+	/* Finally register the compute() callback. */
+	p_init_compute(compute);
+}
