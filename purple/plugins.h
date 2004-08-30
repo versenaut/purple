2bbe662e9d848ad6840a69b76b8ec88086d5ba14
Emil Brisk <emil.brink@interactiveinstitute.se>
2004-08-30 10:27:02 +0000

    Renamed inputset to portset
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@655 49c17534-1d02-dc11-b283-0030488c597c

--- purple/plugins.h
+++ purple/plugins.h
@@ -8,24 +8,21 @@
 
 #include "purple.h"
 
-typedef struct Library		Library;
-typedef struct Plugin		Plugin;
+typedef struct Library	Library;
+typedef struct Plugin	Plugin;
 
-typedef struct PInputSet	PInputSet;
+typedef struct PPortSet	PPortSet;
 
 typedef struct
 {
-	PInputSet	*inputs;
+	PPortSet	*inputs;
 	void		*state;
 } PInstance;
 
 extern void	plugins_init(const char *paths);
 
-extern const char *	plugin_input_type_to_name(PInputType t);
-extern PInputType	plugin_input_type_from_name(const char *name);
-
 extern Plugin *	plugin_new(const char *name);
-extern void	plugin_set_input(Plugin *p, int index, PInputType type, const char *name, va_list taglist);
+extern void	plugin_set_input(Plugin *p, int index, PValueType type, const char *name, va_list taglist);
 extern void	plugin_set_meta(Plugin *p, const char *category, const char *text);
 extern void	plugin_set_state(Plugin *p, size_t size, void (*ctor)(void *state), void (*dtor)(void *state));
 extern void	plugin_set_compute(Plugin *p, void (*compute)(PPInput *input, PPOutput output, void *state));
@@ -44,15 +41,15 @@ extern const Plugin *	plugin_lookup(unsigned int id);
 extern unsigned int	plugin_id(const Plugin *p);
 extern const char *	plugin_name(const Plugin *p);
 
-extern PInputSet *	plugin_inputset_new(const Plugin *p);
-extern PPInput *	plugin_inputset_ports(PInputSet *is);
-extern void		plugin_inputset_set_va(PInputSet *is, unsigned int index, PInputType type, va_list arg);
-extern void		plugin_inputset_clear(PInputSet *is, unsigned int index);
-extern boolean		plugin_inputset_is_set(const PInputSet *is, unsigned int index);
-extern size_t		plugin_inputset_size(const PInputSet *is);
-extern boolean		plugin_inputset_get_module(const PInputSet *is, unsigned int index, uint32 *module_id);
-extern void		plugin_inputset_describe(const PInputSet *is, DynStr *d);
-extern void		plugin_inputset_destroy(PInputSet *is);
+extern PPortSet *	plugin_portset_new(const Plugin *p);
+extern PPInput *	plugin_portset_ports(PPortSet *is);
+extern void		plugin_portset_set_va(PPortSet *is, unsigned int index, PValueType type, va_list arg);
+extern void		plugin_portset_clear(PPortSet *is, unsigned int index);
+extern boolean		plugin_portset_is_set(const PPortSet *is, unsigned int index);
+extern size_t		plugin_portset_size(const PPortSet *is);
+extern boolean		plugin_portset_get_module(const PPortSet *is, unsigned int index, uint32 *module_id);
+extern void		plugin_portset_describe(const PPortSet *is, DynStr *d);
+extern void		plugin_portset_destroy(PPortSet *is);
 
 extern int		plugin_instance_init(Plugin *p, PInstance *inst);
 extern void		plugin_instance_compute(Plugin *p, const PInstance *inst);
