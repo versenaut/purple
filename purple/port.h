ec0892397379c596abc3202b126f6e6e4bbbc090
Emil Brisk <emil.brink@interactiveinstitute.se>
2004-08-30 10:24:25 +0000

    Added API declarations
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@653 49c17534-1d02-dc11-b283-0030488c597c

--- purple/port.h
+++ purple/port.h
@@ -4,9 +4,21 @@
 
 typedef struct PNodes	PNodes;
 
-typedef struct {
+typedef struct PPort {
 	PValue	value;		/* Values are written (set) into here. */
 	PValue	cache;		/* Holds cached values, i.e. transforms between types. */
 	PNodes	*nodes;		/* Simply NULL if no nodes present. Not worth optimizing out of struct. */
 } PPort;
 
+extern void	port_init(PPort *port);
+extern void	port_clear(PPort *port);
+
+extern boolean	port_is_unset(const PPort *port);
+extern boolean	port_peek_module(const PPort *port, uint32 *module_id);
+
+extern void	port_append_value(const PPort *port, DynStr *d);
+
+extern int	port_set_va(PPort *port, PValueType type, va_list arg);
+
+extern boolean		port_input_boolean(PPort *port);
+extern const char *	port_input_string(PPort *port);
