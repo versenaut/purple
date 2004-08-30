bd8ab2c3f993b20053300e86ef3f3b56358541de
Emil Brisk <emil.brink@interactiveinstitute.se>
2004-08-30 10:33:37 +0000

    Adapted for changes in input API
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@658 49c17534-1d02-dc11-b283-0030488c597c

--- purple/plugins/hello.c
+++ purple/plugins/hello.c
@@ -6,7 +6,7 @@
 #include "purple-plugin.h"
 
 /* It's more fun to compute. */
-static void compute(PPInput *input, PPOutput output, void *state)
+static void compute(PPInput *input, PPOutput *output, void *state)
 {
 	printf("Hello!\n");
 }
@@ -14,13 +14,13 @@ static void compute(PPInput *input, PPOutput output, void *state)
 void init(void)
 {
 	p_init_create("hello");
-	p_init_input(0, P_INPUT_INT32, "mode",
+	p_init_input(0, P_VALUE_INT32, "mode",
 		     P_INPUT_MIN(0),
 		     P_INPUT_MAX(17),
 		     P_INPUT_DEFAULT(5),
 		     P_INPUT_REQUIRED,
 		     P_INPUT_DONE);
-	p_init_input(1, P_INPUT_STRING, "text", P_INPUT_DEFAULT_STR("monster"), P_INPUT_DONE);
+	p_init_input(1, P_VALUE_STRING, "text", P_INPUT_DEFAULT_STR("monster"), P_INPUT_DONE);
 	p_init_meta("desc/purpose", "Say hello to the world");
 	p_init_meta("author", "Emil Brink");
 	p_init_compute(compute);
--- purple/plugins/math.c
+++ purple/plugins/math.c
@@ -9,11 +9,10 @@ static void compute_add_real32(PPInput *input, PPOutput output, void *state)
 	real32	a, b;
 
 	{
-		char	abuf[256], bbuf[256];
-
-		if(p_input_string(input[0], abuf, sizeof abuf) &&
-		   p_input_string(input[1], bbuf, sizeof bbuf))
-			printf("Inputs are '%s' and '%s'\n", abuf, bbuf);
+		const char	*as, *bs;
+		if((as = p_input_string(input[0])) != NULL &&
+		   (bs = p_input_string(input[1])) != NULL)
+			printf("Inputs are '%s' and '%s'\n", as, bs);
 	}
 	a = p_input_real32(input[0]);
 	b = p_input_real32(input[1]);
@@ -34,15 +33,15 @@ static void compute_sub_real32(PPInput *input, PPOutput output, void *state)
 void init(void)
 {
 	p_init_create("add_r32");
-	p_init_input(0, P_INPUT_REAL32, "a", P_INPUT_REQUIRED, P_INPUT_DONE);
-	p_init_input(1, P_INPUT_REAL32, "b", P_INPUT_REQUIRED, P_INPUT_DONE);
+	p_init_input(0, P_VALUE_REAL32, "a", P_INPUT_REQUIRED, P_INPUT_DONE);
+	p_init_input(1, P_VALUE_REAL32, "b", P_INPUT_REQUIRED, P_INPUT_DONE);
 	p_init_meta("desc/purpose", "Adds two real32 numbers together");
 	p_init_meta("author", "Emil Brink");
 	p_init_compute(compute_add_real32);
 
 	p_init_create("sub_r32");
-	p_init_input(0, P_INPUT_REAL32, "a", P_INPUT_REQUIRED, P_INPUT_DONE);
-	p_init_input(1, P_INPUT_REAL32, "b", P_INPUT_REQUIRED, P_INPUT_DONE);
+	p_init_input(0, P_VALUE_REAL32, "a", P_INPUT_REQUIRED, P_INPUT_DONE);
+	p_init_input(1, P_VALUE_REAL32, "b", P_INPUT_REQUIRED, P_INPUT_DONE);
 	p_init_meta("desc/purpose", "Computes difference of two real32 numbers");
 	p_init_meta("author", "Emil Brink");
 	p_init_compute(compute_sub_real32);
