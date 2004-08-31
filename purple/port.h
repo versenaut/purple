40edd5339e8adf0a6db33e35b4f8e0d350b48893
Emil Brisk <emil.brink@interactiveinstitute.se>
2004-08-31 08:05:46 +0000

    Added missing accessor prototypes
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@676 49c17534-1d02-dc11-b283-0030488c597c

--- purple/port.h
+++ purple/port.h
@@ -22,4 +22,16 @@ extern void		port_append_value(const PPort *port, DynStr *d);
 extern int		port_set_va(PPort *port, PValueType type, va_list arg);
 
 extern boolean		port_input_boolean(PPort *port);
+extern int32		port_input_int32(PPort *port);
+extern uint32		port_input_uint32(PPort *port);
+extern real32		port_input_real32(PPort *port);
+extern const real32 *	port_input_real32_vec2(PPort *port);
+extern const real32 *	port_input_real32_vec3(PPort *port);
+extern const real32 *	port_input_real32_vec4(PPort *port);
+extern const real32 *	port_input_real32_mat16(PPort *port);
+extern const real64 *	port_input_real64_vec2(PPort *port);
+extern const real64 *	port_input_real64_vec3(PPort *port);
+extern const real64 *	port_input_real64_vec4(PPort *port);
+extern const real64 *	port_input_real64_mat16(PPort *port);
 extern const char *	port_input_string(PPort *port);
+extern uint32		port_input_module(PPort *port);
