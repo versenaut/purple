83ed0cf6ccfae4c057581f192569cfffa8f6575a
Emil Brisk <emil.brink@interactiveinstitute.se>
2004-08-31 07:19:06 +0000

    Indent fixes
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@667 49c17534-1d02-dc11-b283-0030488c597c

--- purple/port.h
+++ purple/port.h
@@ -10,15 +10,16 @@ typedef struct PPort {
 	PNodes	*nodes;		/* Simply NULL if no nodes present. Not worth optimizing out of struct. */
 } PPort;
 
-extern void	port_init(PPort *port);
-extern void	port_clear(PPort *port);
+extern void		port_init(PPort *port);
+extern void		port_clear(PPort *port);
 
-extern boolean	port_is_unset(const PPort *port);
-extern boolean	port_peek_module(const PPort *port, uint32 *module_id);
+extern boolean		port_is_unset(const PPort *port);
+extern boolean		port_peek_module(const PPort *port, uint32 *module_id);
+extern const char *	port_get_type_name(const PPort *port);
 
-extern void	port_append_value(const PPort *port, DynStr *d);
+extern void		port_append_value(const PPort *port, DynStr *d);
 
-extern int	port_set_va(PPort *port, PValueType type, va_list arg);
+extern int		port_set_va(PPort *port, PValueType type, va_list arg);
 
 extern boolean		port_input_boolean(PPort *port);
 extern const char *	port_input_string(PPort *port);
