62107ab63ed78b9911620da591fc9e038759aaba
Emil Brisk <emil.brink@interactiveinstitute.se>
2004-09-03 14:00:29 +0000

    Test strdup() corners; test strdup() with maxlen
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@739 49c17534-1d02-dc11-b283-0030488c597c

--- purple/tests/test-strutil.c
+++ purple/tests/test-strutil.c
@@ -14,7 +14,7 @@ int main(void)
 {
 	test_package_begin("strutil", "String utility functions");
 
-	test_begin("strdup");
+	test_begin("strdup()");
 	{
 		char	*a, *b;
 
@@ -27,7 +27,36 @@ int main(void)
 	}
 	test_end();
 
-	test_begin("strncpy");
+	test_begin("strdup() corners");
+	{
+		char	*a, *b;
+		int	ok = 0;
+
+		a = stu_strdup(NULL);
+		b = stu_strdup(NULL);
+		if(a == NULL && b == NULL)
+		{
+			a = strdup("");
+			b = strdup("");
+			if(a != b && a != NULL && b != NULL && *a == *b && *a == '\0')
+				ok = 1;
+			mem_free(a);
+			mem_free(b);
+		}
+		test_result(ok);
+	}
+	test_end();
+
+	test_begin("strdup() with maxlen");
+	{
+		char	*a;
+
+		a = stu_strdup_maxlen("foobar", 3);
+		test_result(strcmp(a, "fo") == 0);
+	}
+	test_end();
+
+	test_begin("strncpy()");
 	{
 		char	dest[4],
 			src[4] = "HELO";	/* Does not get terminator. */
