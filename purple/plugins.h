2ec769c4024949a25d9a650082c6e4b8cc26af04
Emil Brisk <emil.brink@interactiveinstitute.se>
2004-09-09 08:52:39 +0000

    Instance has plug-in pointer; added status
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@775 49c17534-1d02-dc11-b283-0030488c597c

--- purple/plugins.h
+++ purple/plugins.h
@@ -17,6 +17,7 @@ typedef struct PPortSet	PPortSet;
 
 typedef struct
 {
+	Plugin		*plugin;
 	PPortSet	*inputs;
 	PPOutput	output;
 	void		*state;
@@ -33,7 +34,7 @@ extern Plugin *	plugin_new(const char *name);
 extern void	plugin_set_input(Plugin *p, int index, PValueType type, const char *name, va_list taglist);
 extern void	plugin_set_meta(Plugin *p, const char *category, const char *text);
 extern void	plugin_set_state(Plugin *p, size_t size, void (*ctor)(void *state), void (*dtor)(void *state));
-extern void	plugin_set_compute(Plugin *p, void (*compute)(PPInput *input, PPOutput output, void *state));
+extern void	plugin_set_compute(Plugin *p, PComputeStatus (*compute)(PPInput *input, PPOutput output, void *state));
 extern char *	plugin_describe(const Plugin *p);
 extern void	plugin_describe_append(const Plugin *p, DynStr *ds);
 extern void	plugin_destroy(Plugin *p);
@@ -63,6 +64,15 @@ extern boolean		plugin_portset_get_module(const PPortSet *ps, unsigned int index
 extern void		plugin_portset_describe(const PPortSet *ps, DynStr *d);
 extern void		plugin_portset_destroy(PPortSet *ps);
 
+typedef enum
+{
+	PLUGIN_RETRY_INCOMPLETE = -1,			/* Computation is proceeding, please call again. */
+	PLUGIN_STOP = 0,				/* General stopping condition, all values >= this mean stop. */
+	PLUGIN_STOP_COMPLETE = PLUGIN_STOP,		/* Computation is done! */
+	PLUGIN_STOP_INPUT_MISSING,			/* Required inputs are missing, no go. */
+	PLUGIN_STOP_FAILURE,				/* Generic, probably permanent, failure occured. */
+} PluginStatus;
+
 /* Plug-in instances are used to represent instances of plug-ins ("modules"). It's a
  * transparent structure holding an inputset and a state pointer, basically.
 */
@@ -70,5 +80,5 @@ extern int		plugin_instance_init(Plugin *p, PInstance *inst);
 extern void		plugin_instance_set_output(PInstance *inst, PPOutput output);
 extern void		plugin_instance_set_link_resolver(PInstance *inst,
 							  PPOutput (*get_output)(uint32 module_id, void *data), void *data);
-extern void		plugin_instance_compute(Plugin *p, PInstance *inst);
-extern void		plugin_instance_free(Plugin *p, PInstance *inst);
+extern PluginStatus	plugin_instance_compute(PInstance *inst);
+extern void		plugin_instance_free(PInstance *inst);
