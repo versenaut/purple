bd72ff498c5e1a18675455c2a6be129540e9a9ab
Emil Brisk <emil.brink@interactiveinstitute.se>
2004-09-16 06:23:50 +0000

    Pass state to notify-function
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@818 49c17534-1d02-dc11-b283-0030488c597c

--- purple/plugin-input.c
+++ purple/plugin-input.c
@@ -62,7 +62,7 @@ static PComputeStatus compute(PPInput *input, PPOutput output, void *state_typel
 			/* De-register current. */
 			nodedb_notify_node_remove(state->notify, state->notify_handle);
 			if(node != NULL)	/* Register new. */
-				state->notify_handle = nodedb_notify_node_add(node, cb_notify, NULL);
+				state->notify_handle = nodedb_notify_node_add(node, cb_notify, state);
 			else
 				state->notify_handle = NULL;
 			state->notify = node;
