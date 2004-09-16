d8e5e67341f71161ff13d310b374c5d49df6ac73
Emil Brisk <emil.brink@interactiveinstitute.se>
2004-09-16 06:22:25 +0000

    Added new_copy() and destroy()
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@816 49c17534-1d02-dc11-b283-0030488c597c

--- purple/nodedb.h
+++ purple/nodedb.h
@@ -15,7 +15,8 @@ typedef struct
 	DynArr		*tags;
 } TagGroup;
 
-typedef struct
+/* This is typedef:ed to Node in the public purple.h header. */
+struct Node
 {
 	VNodeID		id;
 	VNodeType	type;
@@ -24,7 +25,9 @@ typedef struct
 	DynArr		*tag_groups;
 
 	List		*notify;
-} Node;
+
+	Node *		(*copy)(const Node *n);
+};
 
 #include "nodedb-o.h"
 #include "nodedb-t.h"
@@ -35,6 +38,12 @@ extern Node *		nodedb_lookup(VNodeID node_id);
 extern Node *		nodedb_lookup_by_name(const char *name);
 extern NodeObject *	nodedb_lookup_object(VNodeID node_id);	/* Convenient. */
 extern NodeText *	nodedb_lookup_text(VNodeID node_id);
+
+extern Node *		nodedb_new(VNodeType type);
+extern Node *		nodedb_new_copy(const Node *src);
+extern void		nodedb_destroy(Node *n);
+
+extern void		nodedb_rename(Node *node, const char *name);
 
 typedef enum { NODEDB_OWNERSHIP_ALL, NODEDB_OWNERSHIP_MINE, NODEDB_OWNERSHIP_OTHERS } NodeOwnership;
 typedef enum { NODEDB_NOTIFY_CREATE, NODEDB_NOTIFY_STRUCTURE, NODEDB_NOTIFY_DATA, NODEDB_NOTIFY_DESTROY } NodeNotifyEvent;
