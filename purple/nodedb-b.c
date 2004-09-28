5dbd5a023c9e909aa505c432db168f4960773535
Emil Brisk <emil.brink@interactiveinstitute.se>
2004-09-28 10:37:58 +0000

    Bitmap node databasing
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@1002 49c17534-1d02-dc11-b283-0030488c597c

---
+++ purple/nodedb-b.c
@@ -0,0 +1,41 @@
+/*
+ * Bitmap node databasing.
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+#include "verse.h"
+#include "purple.h"
+
+#include "dynarr.h"
+#include "list.h"
+#include "mem.h"
+#include "strutil.h"
+#include "textbuf.h"
+
+#include "nodedb.h"
+#include "nodedb-internal.h"
+
+/* ----------------------------------------------------------------------------------------- */
+
+void nodedb_b_construct(NodeBitmap *n)
+{
+}
+
+void nodedb_b_copy(NodeBitmap *n, const NodeBitmap *src)
+{
+}
+
+void nodedb_b_destruct(NodeBitmap *n)
+{
+}
+
+/* ----------------------------------------------------------------------------------------- */
+
+
+/* ----------------------------------------------------------------------------------------- */
+
+void nodedb_b_register_callbacks(void)
+{
+}
---
+++ purple/nodedb-b.h
@@ -0,0 +1,27 @@
+/*
+ * Bitmap node database implementation.
+*/
+
+typedef struct
+{
+	uint16		id;
+	char		name[16];
+	VNBLayerType	type;
+	VNBTile		**tiles;	/* Cube of tile pointers. In "XYZ" order. */
+} NdbBLayer;
+
+typedef struct
+{
+	Node	node;
+	uint16	width, height, depth;
+	DynArr	*layers;
+} NodeBitmap;
+
+extern void		nodedb_b_construct(NodeBitmap *n);
+extern void		nodedb_b_copy(NodeBitmap *n, const NodeBitmap *src);
+extern void		nodedb_b_destruct(NodeBitmap *n);
+
+extern NdbBLayer *	nodedb_b_layer_lookup(const NodeBitmap *node, const char *name);
+extern NdbBLayer *	nodedb_b_layer_lookup_id(const NodeBitmap *node, uint16 buffer_id);
+
+extern void		nodedb_b_register_callbacks(void);
