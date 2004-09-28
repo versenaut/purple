8697eaeab7c6d02556f98b5c87eaabb2f1717aae
Emil Brisk <emil.brink@interactiveinstitute.se>
2004-09-28 10:53:46 +0000

    Callback skeletons
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@1004 49c17534-1d02-dc11-b283-0030488c597c

--- purple/nodedb-b.c
+++ purple/nodedb-b.c
@@ -21,10 +21,16 @@
 
 void nodedb_b_construct(NodeBitmap *n)
 {
+	n->width  = n->height = n->depth = 0U;
+	n->layers = NULL;
 }
 
 void nodedb_b_copy(NodeBitmap *n, const NodeBitmap *src)
 {
+	n->width  = src->width;
+	n->height = src->height;
+	n->depth  = src->depth;
+	/* FIXME: Copy layers. */
 }
 
 void nodedb_b_destruct(NodeBitmap *n)
@@ -33,9 +39,28 @@ void nodedb_b_destruct(NodeBitmap *n)
 
 /* ----------------------------------------------------------------------------------------- */
 
+static void cb_b_dimensions_set(VNodeID node_id, uint16 width, uint16 height, uint16 depth)
+{
+}
+
+static void cb_b_layer_create(VNodeID node_id, VLayerID layer_id, const char *name, VNBLayerType type)
+{
+}
+
+static void cb_b_layer_destroy(VNodeID node_id, VLayerID layer_id)
+{
+}
+
+static void cb_b_tile_set(VNodeID node_id, VLayerID layer_id, uint16 tile_x, uint16 tile_y, uint16 tile_z, VNBLayerType type, VNBTile *tile)
+{
+}
 
 /* ----------------------------------------------------------------------------------------- */
 
 void nodedb_b_register_callbacks(void)
 {
+	verse_callback_set(verse_send_b_dimensions_set,	cb_b_dimensions_set, NULL);
+	verse_callback_set(verse_send_b_layer_create,	cb_b_layer_create, NULL);
+	verse_callback_set(verse_send_b_tile_set,	cb_b_tile_set, NULL);
+	verse_callback_set(verse_send_b_layer_destroy,	cb_b_layer_destroy, NULL);
 }
