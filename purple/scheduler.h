882e021907b5e52d0cf1cf7bb4ed39ba149c4b4b
Emil Brisk <emil.brink@interactiveinstitute.se>
2004-09-28 08:09:56 +0000

    More comments
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@995 49c17534-1d02-dc11-b283-0030488c597c

--- purple/scheduler.h
+++ purple/scheduler.h
@@ -1,6 +1,16 @@
 /*
- * 
+ * The scheduler module is responsible for making plug-in code run, by calling (indirectly)
+ * their compute() entry points.
+*/
+
+/* Add the <inst> instance to the set of plug-ins that need to be run. The only way to stop
+ * running is to return P_COMPUTE_DONE from compute(), there is no remove().
 */
 
 extern void	sched_add(PInstance *inst);
+
+/* Give the scheduler CPU time to spend running plug-ins. It will time itself and stop
+ * running code after a (currently hard-coded) time has passed. This is only co-operative
+ * however, no preemption is done.
+*/
 extern void	sched_update(void);
