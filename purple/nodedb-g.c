d52ea25a1cc61e2630b42ad79702d5d152cec777
Emil Brisk <emil.brink@interactiveinstitute.se>
2004-10-12 11:28:35 +0000

    Local layer/set functions
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@1092 49c17534-1d02-dc11-b283-0030488c597c

--- purple/nodedb-g.c
+++ purple/nodedb-g.c
@@ -28,6 +28,9 @@ void nodedb_g_construct(NodeGeometry *n)
 	n->crease_vertex.def = 0;
 	n->crease_edge.layer[0] = '\0';
 	n->crease_edge.def = 0;
+
+	nodedb_g_layer_create(n, ~0, "vertex", VN_G_LAYER_VERTEX_XYZ);
+	nodedb_g_layer_create(n, ~0, "polygon", VN_G_LAYER_POLYGON_CORNER_UINT32);
 }
 
 static void cb_copy_layer(void *d, const void *s, void *user)
@@ -58,12 +61,68 @@ void nodedb_g_destruct(NodeGeometry *n)
 		{
 			if((layer = dynarr_index(n->layers, i)) == NULL || layer->name[0] == '\0')
 				continue;
-			mem_free(layer->data);
+			dynarr_destroy(layer->data);
 		}
 		dynarr_destroy(n->layers);
 	}
 	if(n->bones != NULL)
 		dynarr_destroy(n->bones);	/* Bones contain no pointers. */
+}
+
+/* Compare <out> against <in>, and generate up to <cmd_max> commands in <cmd> to alter it if it differs. */
+/*boolean nodedb_g_compare(PVCmd *cmd, size_t cmd_max, const NodeGeometry *in, const NodeGeometry *out)
+{
+	return FALSE;
+}
+*/
+/* ----------------------------------------------------------------------------------------- */
+
+void nodedb_g_layer_create(NodeGeometry *node, VLayerID layer_id, const char *name, VNGLayerType type)
+{
+	NdbGLayer	*layer;
+
+	if(node->layers == NULL)
+		node->layers = dynarr_new(sizeof *layer, 2);
+	if(node->layers == NULL)
+		return;
+	if(layer_id == (uint16) ~0)
+		layer = dynarr_append(node->layers, NULL, NULL);
+	else
+	{
+		if((layer = dynarr_set(node->layers, layer_id, NULL)) != NULL)
+		{
+			if(layer->name[0] != '\0')
+			{
+				if(layer->data != NULL)
+				{
+					dynarr_destroy(layer->data);
+					layer->data = NULL;
+				}
+			}
+		}
+	}
+	layer->id = layer_id;
+	stu_strncpy(layer->name, sizeof layer->name, name);
+	layer->type = type;
+	layer->data = NULL;
+	layer->def_uint = 0;
+	layer->def_real = 0.0;
+	printf("layer '%s' created\n", layer->name);
+}
+
+NdbGLayer * nodedb_g_layer_lookup(const NodeGeometry *node, const char *name)
+{
+	unsigned int	i;
+	NdbGLayer	*layer;
+
+	if(node == NULL)
+		return NULL;
+	for(i = 0; (layer = dynarr_index(node->layers, i)) != NULL; i++)
+	{
+		if(strcmp(layer->name, name) == 0)
+			return layer;
+	}
+	return NULL;
 }
 
 NdbGLayer * nodedb_g_layer_lookup_id(const NodeGeometry *node, VLayerID layer_id)
@@ -93,20 +152,22 @@ static void cb_g_layer_create(void *user, VNodeID node_id, VLayerID layer_id, co
 	printf("now in g_layer_create\n");
 	if((node = (NodeGeometry *) nodedb_lookup_with_type(node_id, V_NT_GEOMETRY)) == NULL)
 		return;
-	if(node->layers == NULL)
-		node->layers = dynarr_new(sizeof *layer, 2);
-	if(node->layers != NULL)
+	if((layer = nodedb_g_layer_lookup(node, name)) != NULL)
 	{
-		if((layer = dynarr_set(node->layers, layer_id, NULL)) != NULL)
+		if(layer->type == type)
 		{
-			if(layer->name[0] != '\0')
-			{
-				if(layer->data != NULL)
-				{
-					dynarr_destroy(layer->data);
-					layer->data = NULL;
-				}
-			}
+			layer->id = layer_id;
+			layer->def_uint = def_uint;
+			layer->def_real = def_real;
+		}
+		else
+			fprintf(stderr, "Missing code here, geo layer needs to be reborn\n");	/* FIXME */
+	}
+	else
+	{
+		nodedb_g_layer_create(node, layer_id, name, type);
+		if((layer = nodedb_g_layer_lookup(node, name)) != NULL)
+		{
 			layer->id = layer_id;
 			stu_strncpy(layer->name, sizeof layer->name, name);
 			layer->type = type;
@@ -115,9 +176,10 @@ static void cb_g_layer_create(void *user, VNodeID node_id, VLayerID layer_id, co
 			layer->def_real = def_real;
 			NOTIFY(node, STRUCTURE);
 			verse_send_g_layer_subscribe(node_id, layer_id, VN_FORMAT_REAL64);
-			printf("created layer %u.%u (%s), type %d\n", node_id, layer_id, name, type);
 		}
 	}
+	if(node->layers == NULL)
+		node->layers = dynarr_new(sizeof *layer, 2);
 }
 
 static void cb_g_layer_destroy(void *user, VNodeID node_id, VLayerID layer_id)
@@ -133,6 +195,21 @@ static void cb_g_layer_destroy(void *user, VNodeID node_id, VLayerID layer_id)
 	if(layer->data != NULL)
 		dynarr_destroy(layer->data);
 	NOTIFY(node, STRUCTURE);
+}
+
+void nodedb_g_vertex_set_xyz(NodeGeometry *node, NdbGLayer *layer, uint32 vertex_id, real64 x, real64 y, real64 z)
+{
+	real64	*vtx;
+
+	if(layer->data == NULL)\
+		layer->data = dynarr_new(3 * sizeof *vtx, 16);	/* FIXME: Should care about defaults. */\
+	if((vtx = dynarr_set(layer->data, vertex_id, NULL)) != NULL)
+	{
+		vtx[0] = x;
+		vtx[1] = y;
+		vtx[2] = z;
+		printf("vertex %u.%u set: (%g,%g,%g)\n", layer->id, vertex_id, x, y, z);
+	}
 }
 
 /* Macro to define a vertex XYZ handler function. */
@@ -189,6 +266,23 @@ VERTEX_SCALAR(real64)
 
 /* Macro to define a polygon corner value handler function. */
 #define POLYGON_CORNER(t)	\
+	void nodedb_g_polygon_set_corner_ ##t(NodeGeometry *node, NdbGLayer *layer, uint32 polygon_id,\
+						      t v0, t v1, t v2, t v3)\
+	{\
+		t		*v;\
+		\
+		if(layer->data == NULL)\
+			layer->data = dynarr_new(4 * sizeof *v, 16);	/* FIXME: Should care about defaults. */\
+		if((v = dynarr_set(layer->data, polygon_id, NULL)) != NULL)\
+		{\
+			v[0] = v0;\
+			v[1] = v1;\
+			v[2] = v2;\
+			v[3] = v3;\
+			NOTIFY(node, DATA);\
+		}\
+	}\
+	\
 	static void cb_g_polygon_set_corner_ ##t(void *user, VNodeID node_id, VLayerID layer_id, uint32 polygon_id,\
 							 t v0, t v1, t v2, t v3)\
 	{\
@@ -302,14 +396,14 @@ static void cb_g_crease_set_edge(void *user, VNodeID node_id, const char *layer,
 
 void nodedb_g_register_callbacks(void)
 {
-	verse_callback_set(verse_send_g_layer_create,		cb_g_layer_create,	NULL);
-	verse_callback_set(verse_send_g_layer_destroy,		cb_g_layer_destroy,	NULL);
+	verse_callback_set(verse_send_g_layer_create,			cb_g_layer_create,	NULL);
+	verse_callback_set(verse_send_g_layer_destroy,			cb_g_layer_destroy,	NULL);
 
-	verse_callback_set(verse_send_g_vertex_set_real32_xyz,	cb_g_vertex_set_real32_xyz,	NULL);
-	verse_callback_set(verse_send_g_vertex_set_real64_xyz,	cb_g_vertex_set_real64_xyz,	NULL);
-	verse_callback_set(verse_send_g_vertex_set_uint32,	cb_g_vertex_set_uint32,		NULL);
-	verse_callback_set(verse_send_g_vertex_set_real32,	cb_g_vertex_set_real32,		NULL);
-	verse_callback_set(verse_send_g_vertex_set_real64,	cb_g_vertex_set_real64,		NULL);
+	verse_callback_set(verse_send_g_vertex_set_real32_xyz,		cb_g_vertex_set_real32_xyz,	NULL);
+	verse_callback_set(verse_send_g_vertex_set_real64_xyz,		cb_g_vertex_set_real64_xyz,	NULL);
+	verse_callback_set(verse_send_g_vertex_set_uint32,		cb_g_vertex_set_uint32,		NULL);
+	verse_callback_set(verse_send_g_vertex_set_real32,		cb_g_vertex_set_real32,		NULL);
+	verse_callback_set(verse_send_g_vertex_set_real64,		cb_g_vertex_set_real64,		NULL);
 
 	verse_callback_set(verse_send_g_polygon_set_corner_uint32,	cb_g_polygon_set_corner_uint32,	NULL);
 	verse_callback_set(verse_send_g_polygon_set_corner_real32,	cb_g_polygon_set_corner_real32,	NULL);
@@ -320,8 +414,8 @@ void nodedb_g_register_callbacks(void)
 	verse_callback_set(verse_send_g_polygon_set_face_real32,	cb_g_polygon_set_face_real32,	NULL);
 	verse_callback_set(verse_send_g_polygon_set_face_real64,	cb_g_polygon_set_face_real64,	NULL);
 
-	verse_callback_set(verse_send_g_bone_create,	cb_g_bone_create, NULL);
+	verse_callback_set(verse_send_g_bone_create,			cb_g_bone_create, NULL);
 
-	verse_callback_set(verse_send_g_crease_set_vertex,	cb_g_crease_set_vertex, NULL);
-	verse_callback_set(verse_send_g_crease_set_vertex,	cb_g_crease_set_edge, NULL);
+	verse_callback_set(verse_send_g_crease_set_vertex,		cb_g_crease_set_vertex, NULL);
+	verse_callback_set(verse_send_g_crease_set_vertex,		cb_g_crease_set_edge, NULL);
 }
