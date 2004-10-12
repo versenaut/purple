f7f44bc1097c7943d37d2297852e3ac669afc098
Emil Brisk <emil.brink@interactiveinstitute.se>
2004-10-12 15:51:27 +0000

    Adding mutates node
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@1107 49c17534-1d02-dc11-b283-0030488c597c

--- purple/synchronizer.h
+++ purple/synchronizer.h
@@ -10,7 +10,7 @@ extern void	sync_init(void);
 /* Request that the given node be synchronized, i.e. compared to local
  * copy and any required changes generated and sent to the Verse server.
 */
-extern void	sync_node_add(const Node *node);
+extern void	sync_node_add(Node *node);
 
 /* Run the synchronizer, attempting not to spend more than <duration> seconds. */
 extern void	sync_update(double slice);
