1decdf3666ccedef1180a947f04d649d2b5fdf5a
Emil Brisk <emil.brink@interactiveinstitute.se>
2004-10-12 12:00:33 +0000

    Prototypes for vertex/polygon functions
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@1097 49c17534-1d02-dc11-b283-0030488c597c

--- purple/nodedb-g.h
+++ purple/nodedb-g.h
@@ -39,4 +39,8 @@ extern void		nodedb_g_destruct(NodeGeometry *n);
 extern NdbGLayer *	nodedb_g_layer_lookup(const NodeGeometry *n, const char *name);
 extern void		nodedb_g_layer_create(NodeGeometry *n, VLayerID layer_id, const char *name, VNGLayerType type);
 
+extern void		nodedb_g_vertex_set_xyz(NodeGeometry *node, NdbGLayer *layer, uint32 vertex_id, real64 x, real64 y, real64 z);
+extern void		nodedb_g_polygon_set_corner_uint32(NodeGeometry *node, NdbGLayer *layer, uint32 polygon_id,
+							   uint32 v0, uint32 v1, uint32 v2, uint32 v3);
+
 extern void		nodedb_g_register_callbacks(void);
