885841b69cc9067e1472c589ee91c5fa881981ff
Emil Brisk <emil.brink@interactiveinstitute.se>
2004-10-13 13:40:22 +0000

    Initial geometry synchronizing
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@1113 49c17534-1d02-dc11-b283-0030488c597c

--- purple/synchronizer.c
+++ purple/synchronizer.c
@@ -3,16 +3,16 @@
 */
 
 #include <stdio.h>
+#include <string.h>
 
 #include "verse.h"
 
 #include "dynarr.h"
 #include "list.h"
+#include "log.h"
 #include "plugins.h"
 #include "textbuf.h"
 #include "nodedb.h"
-
-#include "command-structs.h"
 
 #include "synchronizer.h"
 
@@ -57,6 +57,125 @@ void sync_init(void)
 
 /* ----------------------------------------------------------------------------------------- */
 
+static void sync_geometry_layer(const NodeGeometry *node, const NdbGLayer *layer, size_t size,
+				const NodeGeometry *target, const NdbGLayer *tlayer, size_t tsize,
+				size_t common)
+{
+	const unsigned char	*data, *tdata;
+	size_t		i, esize;
+	int		send;
+
+	esize = tlayer->type == VN_G_LAYER_VERTEX_XYZ ? (3 * sizeof (real64)) : (4 * sizeof (uint32));
+
+	data  = dynarr_index(layer->data, 0);
+	tdata = dynarr_index(tlayer->data, 0);
+	for(i = 0; i < size; i++)
+	{
+		if(i >= tsize)					/* If data is not even in target, we must send it. */
+			send = TRUE;
+		else
+			send = memcmp(data, tdata, esize) != 0;	/* If it fits, compare to see if send needed. */
+		if(send)
+		{
+			switch(tlayer->type)
+			{
+			case VN_G_LAYER_VERTEX_XYZ:
+				verse_send_g_vertex_set_real64_xyz(target->node.id, tlayer->id, i,
+							      ((real64 *) data)[0],
+							      ((real64 *) data)[1], 
+							      ((real64 *) data)[2]);
+				break;
+			case VN_G_LAYER_POLYGON_CORNER_UINT32:
+				verse_send_g_polygon_set_corner_uint32(target->node.id, tlayer->id, i,
+								       ((uint32 *) data)[0],
+								       ((uint32 *) data)[1],
+								       ((uint32 *) data)[2],
+								       ((uint32 *) data)[3]);
+				break;
+			default:
+				;
+			}
+		}
+		data  += esize;
+		tdata += esize;
+	}
+
+	if(size < tsize)	/* We have less data than the target, so delete remainder. */
+	{
+		printf(" target is too large, deleting\n");
+		if(layer->type >= VN_G_LAYER_VERTEX_XYZ && layer->type < VN_G_LAYER_POLYGON_CORNER_UINT32)
+		{
+			for(i = size; i < tsize; i++)
+				verse_send_g_vertex_delete_real64(target->node.id, i);
+		}
+		else
+		{
+			for(i = size; i < tsize; i++)
+				verse_send_g_polygon_delete(target->node.id, i);
+		}
+	}
+}
+
+static void sync_geometry(const NodeGeometry *n, const NodeGeometry *target)
+{
+	unsigned int	i;
+	const NdbGLayer	*layer, *tlayer;
+
+	/* Step one: see if desired layers exist in target, else create them. */
+	for(i = 0; ((layer = dynarr_index(n->layers, i)) != NULL); i++)
+	{
+		if(layer->name[0] == '\0')
+			continue;
+/*		printf("does target have '%s'?\n", layer->name);*/
+		if((tlayer = nodedb_g_layer_lookup(target, layer->name)) != NULL)
+		{
+/*			printf(" yes\n");*/
+			if(layer->type != tlayer->type)
+				printf("  but the type is wrong :/\n");
+			else if(layer->def_uint != tlayer->def_uint)
+				printf("  but the default integer is wrong\n");
+			else if(layer->def_real != tlayer->def_real)
+				printf("  but the default real is wrong\n");
+			else	/* "Envelope" is fine, inspect contents. */
+			{
+				size_t	len, tlen, clen, samelen;
+
+				/* Vertex data must be identical for this to pass; order is significant. */
+				len  = dynarr_size(layer->data);
+				tlen = dynarr_size(tlayer->data);
+/*				clen = dynarr_size_common_prefix(layer->data, tlayer->data);
+				printf(" common prefix length is %u\n", clen);
+				if(clen == len && clen == tlen)
+				{
+					printf(" all done\n");
+					return;
+				}
+*/				sync_geometry_layer(n, layer, len, target, tlayer, tlen, 0);
+			}
+		}
+		else
+		{
+			verse_send_g_layer_create(target->node.id, ~0, layer->name, layer->type, layer->def_uint, layer->def_real);
+			printf(" no, we need to create it\n");
+		}
+	}
+}
+
+static void sync_node(Node *n)
+{
+	Node	*target;
+
+	if((target = nodedb_lookup(n->id)) == NULL)
+	{
+		LOG_WARN(("Couldn't look up existing (target) node for %u--aborting sync", n->id));
+		return;
+	}
+	if(n->type == V_NT_GEOMETRY)
+		sync_geometry((NodeGeometry *) n, (NodeGeometry *) target);
+}
+
+/* ----------------------------------------------------------------------------------------- */
+
 void sync_node_add(Node *node)
 {
 	if(node == NULL)
@@ -90,5 +209,7 @@ void sync_update(double slice)
 	for(iter = sync_info.queue_sync; iter != NULL; iter = list_next(iter))
 	{
 		Node	*n = list_data(iter);
+
+		sync_node(n);
 	}
 }
