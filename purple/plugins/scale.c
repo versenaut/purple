c876f30cf49b1f1b7f15dbd86424f31c89b9d5da
Emil Brisk <emil.brink@interactiveinstitute.se>
2004-10-14 13:59:44 +0000

    Geometry scaler
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@1136 49c17534-1d02-dc11-b283-0030488c597c

---
+++ purple/plugins/scale.c
@@ -0,0 +1,43 @@
+/*
+ * This plug-in rescales input geometry node.
+*/
+
+#include "purple.h"
+#include "purple-plugin.h"
+
+/* This gets called whenever the input, the scale, changes. Traverse vertices and scale them. */
+static PComputeStatus compute(PPInput *input, PPOutput output, void *state)
+{
+	PINode		*in;
+	PONode		*out;
+	real32		scale = p_input_real32(input[1]);	/* Read out the size. */;
+	size_t		i, j, size;
+
+	for(i = 0; (in = p_input_node_nth(input[0], i)) != NULL; i++)
+	{
+		PONode	*out;
+		PNGLayer *layer;
+
+		if(p_node_type_get(in) != V_NT_GEOMETRY)
+			continue;
+		out = p_output_node(output, in);
+		if((layer = p_node_g_layer_lookup(out, "vertex")) == NULL)
+			continue;
+		size = p_node_g_layer_size(out, layer);
+		for(j = 0; j < size; j++)
+		{
+			real64	x, y, z;
+			p_node_g_vertex_get_xyz(out, layer, j, &x, &y, &z);
+			p_node_g_vertex_set_xyz(out, layer, j, scale * x, scale * y, scale * z);
+		}
+	}
+	return P_COMPUTE_DONE;	/* Sleep until scale changes. */
+}
+
+void init(void)
+{
+	p_init_create("scale");
+	p_init_input(0, P_VALUE_MODULE, "data",  P_INPUT_REQUIRED, P_INPUT_DONE);
+	p_init_input(1, P_VALUE_REAL32, "scale", P_INPUT_REQUIRED, P_INPUT_DONE);
+	p_init_compute(compute);
+}
