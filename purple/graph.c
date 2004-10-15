73bd932b178bf43642d3478c1809af18272a161e
Emil Brisk <emil.brink@interactiveinstitute.se>
2004-10-15 08:13:26 +0000

    Cast
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@1152 49c17534-1d02-dc11-b283-0030488c597c

--- purple/graph.c
+++ purple/graph.c
@@ -397,8 +397,8 @@ PONode * graph_port_output_node_create(PPOutput port, VNodeType type, uint32 lab
 				{
 					if(type == V_NT_GEOMETRY)
 					{
-						nodedb_g_layer_create(*node, ~0, "vertex", VN_G_LAYER_VERTEX_XYZ);
-						nodedb_g_layer_create(*node, ~0, "polygon", VN_G_LAYER_POLYGON_CORNER_UINT32);
+						nodedb_g_layer_create((NodeGeometry *) *node, ~0, "vertex", VN_G_LAYER_VERTEX_XYZ);
+						nodedb_g_layer_create((NodeGeometry *) *node, ~0, "polygon", VN_G_LAYER_POLYGON_CORNER_UINT32);
 					}
 					m->out.nodes.next++;
 					nodedb_ref(*node);
