4b39265b2e5508cb260db44d0ab9f7a3333b8d42
Emil Brisk <emil.brink@interactiveinstitute.se>
2004-10-15 07:16:08 +0000

    Layer size get; vertex xyz get
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@1143 49c17534-1d02-dc11-b283-0030488c597c

--- purple/nodedb-g.h
+++ purple/nodedb-g.h
@@ -37,9 +37,11 @@ extern void		nodedb_g_copy(NodeGeometry *n, const NodeGeometry *src);
 extern void		nodedb_g_destruct(NodeGeometry *n);
 
 extern NdbGLayer *	nodedb_g_layer_lookup(const NodeGeometry *n, const char *name);
+extern size_t		nodedb_g_layer_size(const NodeGeometry *n, const NdbGLayer *layer);
 extern void		nodedb_g_layer_create(NodeGeometry *n, VLayerID layer_id, const char *name, VNGLayerType type);
 
 extern void		nodedb_g_vertex_set_xyz(NodeGeometry *node, NdbGLayer *layer, uint32 vertex_id, real64 x, real64 y, real64 z);
+extern void		nodedb_g_vertex_get_xyz(const NodeGeometry *node, const NdbGLayer *layer, uint32 vertex_id, real64 *x, real64 *y, real64 *z);
 extern void		nodedb_g_polygon_set_corner_uint32(NodeGeometry *node, NdbGLayer *layer, uint32 polygon_id,
 							   uint32 v0, uint32 v1, uint32 v2, uint32 v3);
 
