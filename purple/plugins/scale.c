5d2db1d92dc3696af50143fc7017b3b2f12394f4
Emil Brisk <emil.brink@interactiveinstitute.se>
2004-10-15 07:54:32 +0000

    Simplified slightly
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@1148 49c17534-1d02-dc11-b283-0030488c597c

--- purple/plugins/scale.c
+++ purple/plugins/scale.c
@@ -20,10 +20,9 @@ static PComputeStatus compute(PPInput *input, PPOutput output, void *state)
 
 		if(p_node_type_get(in) != V_NT_GEOMETRY)
 			continue;
-		out = p_output_node(output, in);
-		if((layer = p_node_g_layer_lookup(out, "vertex")) == NULL)
-			continue;
-		size = p_node_g_layer_size(out, layer);
+		out   = p_output_node(output, in);
+		layer = p_node_g_layer_lookup(out, "vertex");
+		size  = p_node_g_layer_size(out, layer);	/* Safely handles NULL layer. */
 		for(j = 0; j < size; j++)
 		{
 			real64	x, y, z;
