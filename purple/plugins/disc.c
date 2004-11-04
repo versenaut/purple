422b09ec50fb056d13fc75e5f61bc8ea71b5497c
Emil Brisk <emil.brink@interactiveinstitute.se>
2004-11-04 15:29:58 +0000

    Simple disc bitmap generator
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@1325 49c17534-1d02-dc11-b283-0030488c597c

---
+++ purple/plugins/disc.c
@@ -0,0 +1,40 @@
+/*
+ * A bitmap Purple plug-in that creates a filled cirle. Silly.
+*/
+
+#include "purple.h"
+#include "purple-plugin.h"
+
+/* Answer whether (x,y) is within circle centered on a <user>-pixel square.
+ * Uses simplistic integer math only, and thus isn't too precise.
+*/
+static real64 cb_disc(uint32 x, uint32 y, uint32 z, void *user)
+{
+	uint32	size = ((uint32) user) / 2;
+
+	return (x - size) * (x - size) + (y - size) * (y - size) <= size * size;
+}
+
+static PComputeStatus compute(PPInput *input, PPOutput output, void *state)
+{
+	const char	*lname[] = { "col_r", "col_g", "col_b" };
+	PONode		*node;
+	PNBLayer	*layer;
+	uint32		size = p_input_uint32(input[0]), i;
+
+	node = p_output_node_create(output, V_NT_BITMAP, 0);
+	p_node_b_dimensions_set(node, size, size, 1);
+	for(i = 0; i < sizeof lname / sizeof *lname; i++)
+	{
+		layer = p_node_b_layer_create(node, lname[i], VN_B_LAYER_UINT8);
+		p_node_b_layer_foreach_set(node, layer, cb_disc, (void *) size);
+	}
+	return P_COMPUTE_DONE;
+}
+
+void init(void)
+{
+	p_init_create("disc");
+	p_init_input(0, P_VALUE_UINT32, "size", P_INPUT_REQUIRED, P_INPUT_DONE);	/* Size of bitmap to create. */
+	p_init_compute(compute);
+}
