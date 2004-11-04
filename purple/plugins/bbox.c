f30236ac4c0ca456474ca4a0bc85468192688d74
Emil Brisk <emil.brink@interactiveinstitute.se>
2004-11-04 16:05:32 +0000

    Compute bounding box
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@1326 49c17534-1d02-dc11-b283-0030488c597c

---
+++ purple/plugins/bbox.c
@@ -0,0 +1,46 @@
+/*
+ * This plug-in computes the bounding box of input geometry.
+*/
+
+#include "purple.h"
+#include "purple-plugin.h"
+
+/* This gets called whenever the input changes. Traverse vertices and compute bbox. */
+static PComputeStatus compute(PPInput *input, PPOutput output, void *state)
+{
+	PINode		*in;
+	size_t		i, j, k, size;
+	real64		min[4] = { 1E20, 1E20, 1E20, 0.0 }, max[3] = { -1E20, -1E20, -1E20 }, point[3];
+
+	for(i = 0; (in = p_input_node_nth(input[0], i)) != NULL; i++)
+	{
+		const PNGLayer *layer;
+
+		if(p_node_type_get(in) != V_NT_GEOMETRY)
+			continue;
+		layer = p_node_g_layer_lookup(in, "vertex");
+		size = p_node_g_layer_size(in, layer);		/* Safely handles NULL layer. */
+		for(j = 0; j < size; j++)
+		{
+			p_node_g_vertex_get_xyz(in, layer, j, point, point + 1, point + 2);
+			for(k = 0; k < sizeof point / sizeof *point; k++)
+			{
+				if(point[k] > max[k])
+					max[k] = point[k];
+				else if(point[k] < min[k])
+					min[k] = point[k];
+			}
+		}
+		p_output_real64_vec3(output, max);
+		p_output_real64_vec4(output, min);	/* Slightly hackish. */
+		break;		/* Only process first geometry node. */
+	}
+	return P_COMPUTE_DONE;
+}
+
+void init(void)
+{
+	p_init_create("bbox");
+	p_init_input(0, P_VALUE_MODULE, "data", P_INPUT_REQUIRED, P_INPUT_DONE);
+	p_init_compute(compute);
+}
