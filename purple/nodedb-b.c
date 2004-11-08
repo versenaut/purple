3c86f6aee3602547589ef58123c21a090eaebdb4
Emil Brisk <emil.brink@interactiveinstitute.se>
2004-11-08 15:32:04 +0000

    Unbroken comment, argh
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@1337 49c17534-1d02-dc11-b283-0030488c597c

--- purple/nodedb-b.c
+++ purple/nodedb-b.c
@@ -1,4 +1,4 @@
-*
+/*
  * Bitmap node databasing. Bitmaps are stored by allocating contiguous blocks of memory for
  * the required layers, nothing very fancy at all. One-bit-per-pixel layers are stored using
  * 8-bit bytes as the smallest unit of allocation, and never using any single byte for pixels
