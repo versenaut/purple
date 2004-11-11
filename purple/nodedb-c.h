f48222edb36dbefe9fee3366cc54e8bcef6f88c2
Emil Brisk <emil.brink@interactiveinstitute.se>
2004-11-11 07:47:40 +0000

    Curve declarations improved
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@1356 49c17534-1d02-dc11-b283-0030488c597c

--- purple/nodedb-c.h
+++ purple/nodedb-c.h
@@ -2,11 +2,29 @@
  * Curve node databasing.
 */
 
+#define	P_CURVE_MAXDIM	4
+
 typedef struct
 {
-	uint16	id;
-	char	name[16];
-	DynArr	*keys;
+	uint32	pos[P_CURVE_MAXDIM];
+	real64	value[P_CURVE_MAXDIM];
+} NdbCKeyWing;
+
+typedef struct
+{
+	uint32		id;
+	real64		pos;
+	real64		value[P_CURVE_MAXDIM];
+	NdbCKeyWing	pre, post;
+} NdbCKey;
+
+typedef struct
+{
+	VLayerID	id;
+	char		name[16];
+	uint8		dimensions;
+	DynArr		*keys;		/* Array of Keys, actual storage, arranged by ID/index. */
+	List		*curve;		/* List of Keys, ordered by pos. */
 } NdbCCurve;
 
 typedef struct
