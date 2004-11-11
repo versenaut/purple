74bef6cb85de07a6987d84efb02f1f2e26a6f533
Emil Brisk <emil.brink@interactiveinstitute.se>
2004-11-11 12:37:14 +0000

    Simple bitmap alpha blending
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@1371 49c17534-1d02-dc11-b283-0030488c597c

---
+++ purple/plugins/bmblend.c
@@ -0,0 +1,77 @@
+/*
+ * A bitmap Purple plug-in that blends two bitmap nodes together, using standard alpha-blending.
+*/
+
+#include "purple.h"
+#include "purple-plugin.h"
+
+#define	MIN(a, b)	(((a) < (b)) ? (a) : (b))
+
+static PComputeStatus compute(PPInput *input, PPOutput output, void *state)
+{
+	PINode	*in1, *in2;
+	PONode	*out;
+	uint16	dim1[3], dim2[3], width, height, depth;
+	real32	alpha, alphainv;
+	uint8	*fb1 = NULL, *fb2 = NULL, *fbout = NULL;
+
+	in1 = p_input_node(input[0]);
+	in2 = p_input_node(input[1]);
+	if(p_node_type_get(in1) != V_NT_BITMAP || p_node_type_get(in2) != V_NT_BITMAP)
+		return P_COMPUTE_DONE;
+	alpha = p_input_real32(input[2]);
+	if(alpha < 0.0 || alpha > 1.0)
+		return P_COMPUTE_DONE;
+	alphainv = 1.0 - alpha;
+
+	/* Compute output size; only operate on common pixels of inputs. No scaling. */
+	p_node_b_dimensions_get(in1, dim1, dim1 + 1, dim1 + 2);
+	p_node_b_dimensions_get(in2, dim2, dim2 + 1, dim2 + 2);
+	width  = MIN(dim1[0], dim2[0]);
+	height = MIN(dim1[1], dim2[1]);
+	depth  = MIN(dim1[2], dim2[2]);
+
+	/* Create output node. */
+	out = p_output_node_create(output, V_NT_BITMAP, 0);
+	p_node_b_dimensions_set(out, width, height, depth);
+	p_node_b_layer_create(out, "col_r", VN_B_LAYER_UINT8);
+	p_node_b_layer_create(out, "col_g", VN_B_LAYER_UINT8);
+	p_node_b_layer_create(out, "col_b", VN_B_LAYER_UINT8);
+
+	if((fb1 = p_node_b_layer_access_multi_begin(in1, VN_B_LAYER_UINT8, "col_r", "col_g", "col_b", NULL)) != NULL &&
+	   (fb2 = p_node_b_layer_access_multi_begin(in2, VN_B_LAYER_UINT8, "col_r", "col_g", "col_b", NULL)) != NULL &&
+	   (fbout = p_node_b_layer_access_multi_begin(out, VN_B_LAYER_UINT8, "col_r", "col_g", "col_b", NULL)) != NULL)
+	{
+		int	x, y, z;
+
+		printf("blending %ux%ux%u, alpha=%g\n", width, height, depth, alpha);
+		for(z = 0; z < depth; z++)
+		{
+			for(y = 0; y < height; y++)
+			{
+				const uint8	*r1  = fb1 + 3 * z * dim1[0] * dim1[1] + y * 3 * dim1[0],
+						*r2  = fb2 + 3 * z * dim2[0] * dim2[1] + y * 3 * dim2[0];
+				uint8		*put = fbout + 3 * z * width * height + y * 3 * width;
+
+				printf("row %u: %u %u %u\n", y, r1 - fb1, r2 - fb2, put - fbout);
+				for(x = 0; x < 3 * width; x++)
+					*put++ = alpha * *r2++ + alphainv * *r1++;
+			}
+		}
+	}
+	if(fb1 != NULL)
+		p_node_b_layer_access_multi_end(in1, fb1);
+	if(fb2 != NULL)
+		p_node_b_layer_access_multi_end(in2, fb2);
+	if(fbout != NULL)
+		p_node_b_layer_access_multi_end(out, fbout);
+}
+
+void init(void)
+{
+	p_init_create("bmblend");
+	p_init_input(0, P_VALUE_MODULE, "bitmap1", P_INPUT_REQUIRED, P_INPUT_DONE);
+	p_init_input(1, P_VALUE_MODULE, "bitmap2", P_INPUT_REQUIRED, P_INPUT_DONE);
+	p_init_input(2, P_VALUE_REAL32, "alpha",   P_INPUT_REQUIRED, P_INPUT_DONE);
+	p_init_compute(compute);
+}
