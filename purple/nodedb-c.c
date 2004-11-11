4d150075beab149b581d0d91218aa9d2dfa7faef
Emil Brisk <emil.brink@interactiveinstitute.se>
2004-11-11 07:49:59 +0000

    Allocation; key creation implemented
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@1357 49c17534-1d02-dc11-b283-0030488c597c

--- purple/nodedb-c.c
+++ purple/nodedb-c.c
@@ -10,6 +10,7 @@
 
 #include "dynarr.h"
 #include "list.h"
+#include "log.h"
 #include "strutil.h"
 #include "textbuf.h"
 
@@ -59,7 +60,7 @@ void nodedb_c_destruct(NodeCurve *n)
 
 /* ----------------------------------------------------------------------------------------- */
 
-static void cb_c_curve_create(void *user, VNodeID node_id, uint16 curve_id, const char *name)
+static void cb_c_curve_create(void *user, VNodeID node_id, VLayerID curve_id, const char *name, uint8 dimensions)
 {
 	NodeCurve	*n;
 
@@ -67,18 +68,24 @@ static void cb_c_curve_create(void *user, VNodeID node_id, uint16 curve_id, cons
 	{
 		NdbCCurve	*c;
 
+		if(n->curves == NULL)
+			n->curves = dynarr_new(sizeof (NdbCKey), 4);
 		if((c = dynarr_set(n->curves, curve_id, NULL)) != NULL)
 		{
 			c->id = curve_id;
 			stu_strncpy(c->name, sizeof c->name, name);
+			c->dimensions = dimensions;
 			c->keys = NULL;
-			printf("Curve curve %u.%u %s created\n", node_id, curve_id, name);
+			printf("Curve curve %u.%u %s created, dim=%u\n", node_id, curve_id, name, c->dimensions);
 			NOTIFY(n, STRUCTURE);
+			verse_send_c_curve_subscribe(node_id, curve_id);
 		}
 	}
+	else
+		LOG_WARN(("Can't create curve in unknown curve node %u", node_id));
 }
 
-static void cb_c_curve_destroy(void *user, VNodeID node_id, uint16 curve_id)
+static void cb_c_curve_destroy(void *user, VNodeID node_id, VLayerID curve_id)
 {
 	NodeCurve	*n;
 
@@ -97,10 +104,97 @@ static void cb_c_curve_destroy(void *user, VNodeID node_id, uint16 curve_id)
 	}
 }
 
+static void cb_key_default(unsigned int index, void *element, void *user)
+{
+	NdbCKey	*key = element;
+
+	key->id = ~0;
+}
+
+static int cb_key_compare(const void *d1, const void *d2)
+{
+	const NdbCKey	*k1 = d1, *k2 = d2;
+
+	return k1->pos < k2->pos ? -1 : k1->pos > k2->pos;
+}
+
+static void cb_c_key_set(void *user, VNodeID node_id, VLayerID curve_id, uint32 key_id, uint8 dimensions,
+			 real64 *pre_value, uint32 *pre_pos, real64 *value, real64 pos, real64 *post_value, uint32 *post_pos)
+{
+	NodeCurve	*n;
+
+	if((n = (NodeCurve *) nodedb_lookup_with_type(node_id, V_NT_CURVE)) != NULL)
+	{
+		NdbCCurve	*c;
+
+		if((c = dynarr_index(n->curves, curve_id)) != NULL)
+		{
+			if(c->dimensions == dimensions)
+			{
+				NdbCKey	*key;
+
+				if(c->keys == NULL)
+				{
+					c->keys = dynarr_new(sizeof *key, 8);
+					dynarr_set_default_func(c->keys, cb_key_default, NULL);
+				}
+				if((key = dynarr_set(c->keys, key_id, NULL)) != NULL)
+				{
+					int	i, ins = key->id == ~0;
+
+					key->id  = key_id;
+					key->pos = pos;
+					for(i = 0; i < dimensions; i++)
+					{
+						key->value[i]      = value[i];
+						key->pre.pos[i]    = pre_pos[i];
+						key->pre.value[i]  = pre_value[i];
+						key->post.pos[i]   = post_pos[i];
+						key->post.value[i] = post_value[i];
+					}
+					if(ins)
+						c->curve = list_insert_sorted(c->curve, key, cb_key_compare);
+					{
+						const List	*iter;
+
+						printf("Curve: ");
+						for(iter = c->curve; iter != NULL; iter = list_next(iter))
+						{
+							const NdbCKey	*k = list_data(iter);
+							printf(" %g (%u)", k->pos, k->id);
+						}
+						printf("\n");
+					}
+/*					printf("setting curve %u.%u key=%u\n", node_id, curve_id, key_id);
+					printf("  pre:");
+					for(i = 0; i < dimensions; i++)
+					{
+						printf(" (%u,%g)", pre_pos[i], pre_value[i]);
+					}
+					printf("\n");
+					printf("  now: (%g,[", pos);
+					for(i = 0; i < dimensions; i++)
+						printf(" %g", value[i]);
+					printf(" ])\n");
+					printf(" post: ");
+					for(i = 0; i < dimensions; i++)
+						printf(" (%u,%g)", post_pos[i], post_value[i]);
+					printf("\n");
+*/				}
+			}
+			else
+				LOG_WARN(("Got key_set with wrong dimensions, %u is not %u", dimensions, c->dimensions));
+		}
+		else
+			LOG_WARN(("Got c_key_set in unknown curve %u.%u\n", node_id, curve_id));
+	}
+}
+
 /* ----------------------------------------------------------------------------------------- */
 
 void nodedb_c_register_callbacks(void)
 {
 	verse_callback_set(verse_send_c_curve_create,	cb_c_curve_create, NULL);
 	verse_callback_set(verse_send_c_curve_destroy,	cb_c_curve_destroy, NULL);
+	verse_callback_set(verse_send_c_key_set,	cb_c_key_set, NULL);
 }
