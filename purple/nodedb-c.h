d750d04a6f20f6354b3f2cca2caa5408d2b95fe2
Emil Brisk <emil.brink@interactiveinstitute.se>
2004-11-11 09:01:15 +0000

    Max dimensions symbol renamed
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@1364 49c17534-1d02-dc11-b283-0030488c597c

--- purple/nodedb-c.h
+++ purple/nodedb-c.h
@@ -2,19 +2,19 @@
  * Curve node databasing.
 */
 
-#define	P_CURVE_MAXDIM	4
+#define	P_CURVE_DIM_MAX	4
 
 typedef struct
 {
-	uint32	pos[P_CURVE_MAXDIM];
-	real64	value[P_CURVE_MAXDIM];
+	uint32	pos[P_CURVE_DIM_MAX];
+	real64	value[P_CURVE_DIM_MAX];
 } NdbCKeyWing;
 
 typedef struct
 {
 	uint32		id;
 	real64		pos;
-	real64		value[P_CURVE_MAXDIM];
+	real64		value[P_CURVE_DIM_MAX];
 	NdbCKeyWing	pre, post;
 } NdbCKey;
 
