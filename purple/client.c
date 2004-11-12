0d4c70a129220f2fc78bfe21bc9c9896a7444efc
Emil Brisk <emil.brink@interactiveinstitute.se>
2004-11-12 11:25:53 +0000

    Register more node types, including mateiral
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@1379 49c17534-1d02-dc11-b283-0030488c597c

--- purple/client.c
+++ purple/client.c
@@ -146,8 +146,10 @@ static void cb_connect_accept(void *user, VNodeID avatar, void *address, void *c
 		client_info.connection = connection;
 
 		LOG_MSG(("Connected to Verse server, as avatar %u", avatar));
-		nodedb_register_callbacks(avatar, (1 << V_NT_OBJECT) | (1 << V_NT_TEXT) | (1 << V_NT_BITMAP) | (1 << V_NT_GEOMETRY));
-/*		verse_send_node_list(1 << V_NT_OBJECT);*/
+		nodedb_register_callbacks(avatar,
+					  (1 << V_NT_OBJECT) | (1 << V_NT_TEXT) |
+					  (1 << V_NT_BITMAP) | (1 << V_NT_GEOMETRY) |
+					  (1 << V_NT_CURVE)  | (1 << V_NT_MATERIAL));
 		verse_send_node_subscribe(avatar);
 		verse_send_node_name_set(avatar, "PurpleEngine");
 		verse_send_o_method_group_create(avatar, 0, METHOD_GROUP_CONTROL_NAME);
