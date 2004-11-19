41a7620f919b40fcb61e80f3a474ae20e9a2cef9
Emil Brisk <emil.brink@interactiveinstitute.se>
2004-11-19 14:26:58 +0000

    Write output, too
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@1414 49c17534-1d02-dc11-b283-0030488c597c

--- purple/scripts/licglue.py
+++ purple/scripts/licglue.py
@@ -5,6 +5,8 @@
 # A simple Python program to "glue" a license blurb into C code, and also
 # add the filename into each file's top comment. Written by Emil Brink.
 #
+# Hm. This would have been about the same length in C... I must learn more.
+#
 
 import os
 import sys
@@ -28,18 +30,35 @@ def insert(name, file, blurb):
 	file.insert(4, " * ")
 	return file
 
+# Get size of file.
+def filesize(filename):
+	s = os.stat(filename)
+	return s.st_size
+
 # Load a text file, returning its lines as a list.
 def load(filename):
 	f = open(filename, "rt")
 	data = f.read()
 	lines = data.split("\n")
 	f.close()
-	return lines
+	return lines[:len(lines) - 1]
 
 for a in sys.argv[1:]:
 	f = load(a)
 	if len(f) > 0:
+		# Insert blurb.
 		f = insert(a, f, LICENSE)
-		if f == None: continue
-		for i in range(10):
-			print str(i) + ": '" + f[i] + "'"
+		# Save it to temporary file.
+		outname = a + "-blurbed"
+		out = open(outname, "wt")
+		if out != None:
+			for l in f:
+				out.write(l + "\n")
+			out.close()
+			insize = filesize(a)
+			outsize = filesize(outname)
+			# Rename over original.
+			if outsize - insize > len(LICENSE):
+				os.rename(outname, a)
+		else:
+			print "Open failed"
