625460c27f5edf61c86659de118ae4bdde206e76
Emil Brisk <emil.brink@interactiveinstitute.se>
2004-11-26 09:50:14 +0000

    Buffer editing functions
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@1461 49c17534-1d02-dc11-b283-0030488c597c

--- purple/nodedb-t.h
+++ purple/nodedb-t.h
@@ -29,4 +29,8 @@ extern size_t		nodedb_t_buffer_get_count(const NodeText *node);
 extern NdbTBuffer *	nodedb_t_buffer_get_nth(const NodeText *node, unsigned int n);
 extern NdbTBuffer *	nodedb_t_buffer_get_named(const NodeText *node, const char *name);
 
+extern NdbTBuffer *	nodedb_t_buffer_create(NodeText *node, VLayerID buffer_id, const char *name);
+extern void		nodedb_t_buffer_insert(NdbTBuffer *buffer, size_t pos, const char *text);
+extern void		nodedb_t_buffer_delete(NdbTBuffer *buffer, size_t pos, size_t length);
+
 extern void		nodedb_t_register_callbacks(void);
