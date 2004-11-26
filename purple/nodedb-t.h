c19598cece2980cdb3ef050d17b8fc0ba378b28b
Emil Brisk <emil.brink@interactiveinstitute.se>
2004-11-26 15:56:56 +0000

    Added more buffer access calls
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@1474 49c17534-1d02-dc11-b283-0030488c597c

--- purple/nodedb-t.h
+++ purple/nodedb-t.h
@@ -30,7 +30,10 @@ extern NdbTBuffer *	nodedb_t_buffer_get_nth(const NodeText *node, unsigned int n
 extern NdbTBuffer *	nodedb_t_buffer_get_named(const NodeText *node, const char *name);
 
 extern NdbTBuffer *	nodedb_t_buffer_create(NodeText *node, VLayerID buffer_id, const char *name);
+extern char *		nodedb_t_buffer_read_line(NdbTBuffer *buffer, unsigned int line, char *put, size_t putmax);
 extern void		nodedb_t_buffer_insert(NdbTBuffer *buffer, size_t pos, const char *text);
+extern void		nodedb_t_buffer_append(NdbTBuffer *buffer, const char *text);
 extern void		nodedb_t_buffer_delete(NdbTBuffer *buffer, size_t pos, size_t length);
+extern void		nodedb_t_buffer_clear(NdbTBuffer *buffer);
 
 extern void		nodedb_t_register_callbacks(void);
