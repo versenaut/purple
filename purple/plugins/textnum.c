9a8f09dbdf3340d48632583ef054e032a16473b7
Emil Brisk <emil.brink@interactiveinstitute.se>
2004-11-26 10:02:44 +0000

    Silly plug-in to write out numbers in English
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@1465 49c17534-1d02-dc11-b283-0030488c597c

---
+++ purple/plugins/textnum.c
@@ -0,0 +1,92 @@
+/*
+ * Exercise the text node.
+*/
+
+#include "purple.h"
+#include "purple-plugin.h"
+
+static int32 append(int32 part, PNTBuffer *buffer, const char *word)
+{
+	if(part > 0)
+		p_node_t_buffer_append(buffer, " ");
+	p_node_t_buffer_append(buffer, word);
+	return part + 1;
+}
+
+static PComputeStatus compute(PPInput *input, PPOutput output, void *state)
+{
+	PONode		*node;
+	PNTBuffer	*buf;
+	int32		number = p_input_int32(input[0]), part = 0;
+	const char	*digit[] = { "zero", "one", "two", "three", "four",
+				"five", "six", "seven", "eight", "nine" },
+			*teen[] = { "eleven", "twelve", "thirteen", "fourteen", "fifteen",
+				    "sixteen", "seventeen", "eighteen", "nineteen" },
+			*ten[] = { "ten", "twenty", "thirty", "forty", "fifty",
+				"sixty", "seventy", "eighty", "ninety" };
+
+	node = p_output_node_create(output, V_NT_TEXT, 0);
+/*	p_node_t_language_set(node, "English");*/		/* Yes, really. */
+	buf = p_node_t_buffer_create(node, "number");
+	p_node_t_buffer_delete(buf, 0, ~0);		/* I don't exactly rule. :/ */
+	if(number < 0)
+	{
+		p_node_t_buffer_append(buf, "minus ");
+		number = -number;
+	}
+	if(number >= 100000)
+	{
+		append(0, buf, "a lot");
+		return P_COMPUTE_DONE;
+	}
+	do
+	{
+/*		printf("%d: %d\n", part, number);*/
+		if(number == 0)
+			part = append(part, buf, "zero");
+		else if(number < 10)
+		{
+			part = append(part, buf, digit[number]);
+			number = 0;
+		}
+		else if(number > 10 && number < 20)
+		{
+			printf("processing teen\n");
+			if(part > 0)
+				part = append(part, buf, "and");
+			part = append(part, buf, teen[number - 11]);
+			number = 0;
+		}
+		else if(number < 100)
+		{
+			part = append(part, buf, ten[number / 10 - 1]);
+			number %= 10;
+		}
+		else if(number < 1000)
+		{
+			part = append(part, buf, digit[number / 100]);
+			part = append(part, buf, "hundred");
+			number %= 100;
+		}
+		else if(number < 10000)
+		{
+			part = append(part, buf, digit[number / 1000]);
+			part = append(part, buf, "thousand");
+			number %= 1000;
+		}
+		else if(number < 100000)
+		{
+			part = append(part, buf, ten[number / 10000 - 1]);
+			number %= 10000;
+		}
+	} while(number > 0);
+
+	return P_COMPUTE_DONE;
+}
+
+void init(void)
+{
+	p_init_create("textnum");
+	p_init_input(0, P_VALUE_INT32, "number", P_INPUT_REQUIRED, P_INPUT_DONE);
+	p_init_compute(compute);
+}
