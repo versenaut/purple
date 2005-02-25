442146531337b4b3feb4cfe0bf93f3de61986543
Emil Brisk <emil.brink@interactiveinstitute.se>
2005-02-25 16:21:03 +0000

    Better block handling; layer access
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@1929 49c17534-1d02-dc11-b283-0030488c597c

--- purple/nodedb-a.c
+++ purple/nodedb-a.c
@@ -50,6 +50,12 @@ static size_t block_size(VNALayerType type)
 	return size[type];
 }
 
+/* Compare audio blocks, for bintree. */
+static int block_compare(const void *key1, const void *key2)
+{
+	return key1 < key2 ? -1 : key1 > key2;
+}
+
 /* ----------------------------------------------------------------------------------------- */
 
 void nodedb_a_construct(NodeAudio *n)
@@ -57,17 +63,39 @@ void nodedb_a_construct(NodeAudio *n)
 	n->layers = NULL;
 }
 
+static NdbABlk * block_new(const NdbALayer *layer)
+{
+	NdbABlk	*blk;
+
+	if((blk = mem_alloc(sizeof *blk + block_size(layer->type))) != NULL)
+	{
+		blk->type = layer->type;
+		blk->data = blk + 1;
+	}
+	return blk;
+}
+
+static void * block_copy(const void *key, const void *element, void *user)
+{
+	const NdbALayer	*ref = (NdbALayer *) user;
+	NdbABlk		*blk;
+
+	blk = block_new(ref);
+	memcpy(blk->data, ((NdbABlk *) element)->data, block_size(ref->type));
+
+	return blk;
+}
+
 static void cb_copy_layer(void *d, const void *s, void *user)
 {
-#if 0
-	const NdbTBuffer	*src = s;
-	NdbTBuffer		*dst = d;
+	const NdbALayer	*src = s;
+	NdbALayer	*dst = d;
 
 	dst->id = src->id;
 	strcpy(dst->name, src->name);
-	dst->text = textbuf_new(textbuf_length(src->text));
-	textbuf_insert(dst->text, 0, textbuf_text(src->text));
-#endif
+	dst->type = src->type;
+	dst->frequency = src->frequency;
+	dst->blocks = bintree_new_copy(src->blocks, block_copy, dst);
 }
 
 void nodedb_a_copy(NodeAudio *n, const NodeAudio *src)
@@ -113,7 +141,7 @@ NdbALayer * nodedb_a_layer_nth(const NodeAudio *node, unsigned int n)
 	unsigned int	i;
 	NdbALayer	*la;
 
-	if(node == NULL || node->node.type != V_NT_TEXT)
+	if(node == NULL || node->node.type != V_NT_AUDIO)
 		return 0;
 	for(i = 0; (la = dynarr_index(node->layers, i)) != NULL; i++)
 	{
@@ -177,9 +205,163 @@ NdbALayer * nodedb_a_layer_create(NodeAudio *node, VLayerID layer_id, const char
 	}
 	la->id = layer_id;
 	stu_strncpy(la->name, sizeof la->name, name);
+	la->type = type;
+	la->frequency = frequency;
 	la->blocks = NULL;
 
 	return la;
+}
+
+/* ----------------------------------------------------------------------------------------- */
+
+int nodedb_a_blocks_equal(VNALayerType type, const NdbABlk *blk1, const NdbABlk *blk2)
+{
+	if(blk1 == blk2)
+		return 1;
+	if(blk1 == NULL || blk2 == NULL)
+		return 0;
+	return memcmp(blk1->data, blk2->data, block_size(type)) == 0;
+}
+
+/* ----------------------------------------------------------------------------------------- */
+
+#define	READINT(bits)	\
+	{\
+		int ## bits	*get = blk->data;\
+		for(i = 0, get += offset; i < chunk; i++)\
+			*buffer++ = ((real64) *get++) / (real64) (1 << (bits - 1));\
+	}\
+	break
+
+#define	READREAL(bits)	\
+	{\
+		real ## bits	*get = blk->data;\
+		for(i = 0, get += offset; i < chunk; i++)\
+			*buffer++ = *get++;\
+	}\
+	break
+
+unsigned int nodedb_a_layer_read_samples(const NdbALayer *layer, unsigned int start, real64 *buffer, unsigned int length)
+{
+	unsigned int	index, pos, offset, bl, i, chunk, to_go = length, max;
+	const NdbABlk	*blk;
+
+	if(layer == NULL || buffer == NULL || length == 0)
+		return 0;
+	bl = block_len(layer->type);
+
+	max = (unsigned long) bintree_key_maximum(layer->blocks);
+
+/*	printf("Reading out %u samples from position %u (max=%u)\n", length, start, max);*/
+
+	for(pos = start; pos < start + length; pos += chunk, to_go -= chunk)
+	{
+		index  = pos / bl;
+		offset = pos % bl;
+		chunk  = bl - offset;
+		if(chunk > to_go)
+			chunk = to_go;
+
+/*		printf("Getting audio data from position %u -> block %u, offset %u, chunk %u\n", pos, index, offset, chunk);*/
+		if(index > max)
+		{
+/*			printf(" Block index too large, there is no more data. Aborting\n");*/
+			return pos - start;
+		}
+		if((blk = bintree_lookup(layer->blocks, (void *) index)) != NULL)
+		{
+			switch(layer->type)
+			{
+			case VN_A_LAYER_INT8:
+				READINT(8);
+			case VN_A_LAYER_INT16:
+				READINT(16);
+			case VN_A_LAYER_INT24:
+				{
+					uint32	*get = blk->data;
+					printf("  converting 24-bit data (FIXME: BROKEN)\n");	/* FIXME! */
+					for(i = 0, get += offset; i < chunk; i++)
+						*buffer++ = ((real64) *get++) / 8388608.0;
+				}
+				break;
+			case VN_A_LAYER_INT32:
+				READINT(32);
+			case VN_A_LAYER_REAL32:
+				READREAL(32);
+			case VN_A_LAYER_REAL64:
+				READREAL(64);
+			}
+		}
+		else
+		{
+			printf(" Block is clear, setting %u zeroes\n", chunk);
+			for(i = 0; i < chunk; i++)
+				*buffer++ = 0.0;
+		}
+	}
+	return pos - start;
+}
+
+/* Handy dandy macros for writing data back into blocks, and make my life a bit easier. */
+#define	WRITEINT(bits)	\
+	{\
+		int ## bits	*put = blk->data;\
+		for(i = 0, put += offset; i < chunk; i++)\
+			*put++ = *buffer * (real64) (1 << (bits - 1));\
+	}\
+	break
+
+#define	WRITEREAL(bits)	\
+	{\
+		real ## bits	*put = blk->data;\
+		for(i = 0, put += offset; i < chunk; i++)\
+			*put++ = *buffer;\
+	}\
+	break
+
+
+void nodedb_a_layer_write_samples(NdbALayer *layer, unsigned int start, const real64 *buffer, unsigned int length)
+{
+	unsigned int	index, pos, offset, bl, i, chunk, to_go = length;
+	NdbABlk		*blk;
+
+	if(layer == NULL || buffer == NULL || length == 0)
+		return;
+	bl = block_len(layer->type);
+
+	if(layer->blocks == NULL)
+		layer->blocks = bintree_new(block_compare);
+
+/*	printf("Writing back %u samples from position %u\n", length, start);*/
+	for(pos = start, to_go = length; pos < start + length; pos += chunk, to_go -= chunk)
+	{
+		index  = pos / bl;
+		offset = pos % bl;
+		chunk  = bl - offset;
+		if(chunk > to_go)
+			chunk = to_go;
+		
+/*		printf("Setting audio data from position %u -> block %u, offset %u, chunk %u\n", pos, index, offset, chunk);*/
+		if((blk = bintree_lookup(layer->blocks, (void *) index)) == NULL)
+		{
+			printf("no block %u found, creating\n", index);
+			blk = block_new(layer);
+			bintree_insert(layer->blocks, (void *) index, blk);
+		}
+		switch(layer->type)
+		{
+		case VN_A_LAYER_INT8:
+			WRITEINT(8);
+		case VN_A_LAYER_INT16:
+			WRITEINT(16);
+		case VN_A_LAYER_INT32:
+			WRITEINT(32);
+		case VN_A_LAYER_REAL32:
+			WRITEREAL(32);
+		case VN_A_LAYER_REAL64:
+			WRITEREAL(64);
+		}
+	}
 }
 
 /* ----------------------------------------------------------------------------------------- */
@@ -217,14 +399,11 @@ static void cb_a_layer_destroy(void *user, VNodeID node_id, VLayerID layer_id)
 
 		if((al = dynarr_index(n->layers, layer_id)) != NULL)
 		{
+			al->name[0] = '\0';
+			printf("Missing code to destroy audio layer\n");	/* FIXME: Write more. */
 			NOTIFY(n, STRUCTURE);
 		}
 	}
-}
-
-static int block_compare(const void *key1, const void *key2)
-{
-	return key1 < key2 ? -1 : key1 > key2;
 }
 
 static void cb_a_block_set(void *user, VNodeID node_id, VLayerID layer_id, uint32 block_index,
@@ -248,14 +427,13 @@ static void cb_a_block_set(void *user, VNodeID node_id, VLayerID layer_id, uint3
 			{
 				if(blk->type != type)
 				{
-					printf("Got audio block set of type %d on known block of type %d\n", type, blk->type);
+					printf("Got audio block set, index=%u, type %d on known block of type %d\n", block_index, type, blk->type);
 					return;
 				}
 			}
 			else	/* Allocate and insert a new block. */
 			{
-				blk = mem_alloc(sizeof *blk + block_size(type));
-				blk->data = blk + 1;
+				blk = block_new(al);
 				bintree_insert(al->blocks, (void *) block_index, blk);
 				printf("Set audio block %u.%u.%u\n", node_id, layer_id, block_index);
 			}
