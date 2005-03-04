c0c71094de1507c711702b60229f0c3618270a84
Emil Brisk <emil.brink@interactiveinstitute.se>
2005-03-04 09:31:11 +0000

    Header for the resume module
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@1973 49c17534-1d02-dc11-b283-0030488c597c

---
+++ purple/resume.h
@@ -0,0 +1,10 @@
+/*
+ * resume.h
+ * 
+ * Copyright (C) 2005 PDC, KTH. See COPYING for license details.
+ * 
+ * Header for the resume module, declaring its (rather small) interface.
+*/
+
+extern void	resume_init(const char *options);
+extern int	resume_enabled(void);
