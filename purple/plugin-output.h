9b52b621993d7d89ccfdc52309cb86f85183e62c
Emil Brisk <emil.brink@interactiveinstitute.se>
2005-03-04 09:08:45 +0000

    Header for the built-in output plugin
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@1967 49c17534-1d02-dc11-b283-0030488c597c

---
+++ purple/plugin-output.h
@@ -0,0 +1,9 @@
+/*
+ * plugin-output.h
+ * 
+ * Copyright (C) 2005 PDC, KTH. See COPYING for license details.
+ * 
+ * Header for built-in "magical" plug-in "output".
+*/
+
+extern void	plugin_output_init(void);
