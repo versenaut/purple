c8c663c47b7dd8c4c0b462b5c18a5e2a63f7e4ee
Emil Brisk <emil.brink@interactiveinstitute.se>
2005-03-04 11:07:42 +0000

    Added missing read begin/end prototypes
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@1975 49c17534-1d02-dc11-b283-0030488c597c

--- purple/nodedb-t.h
+++ purple/nodedb-t.h
@@ -34,6 +34,8 @@ extern NdbTBuffer *	nodedb_t_buffer_nth(const NodeText *node, unsigned int n);
 extern NdbTBuffer *	nodedb_t_buffer_find(const NodeText *node, const char *name);
 
 extern NdbTBuffer *	nodedb_t_buffer_create(NodeText *node, VLayerID buffer_id, const char *name);
+extern const char *	nodedb_t_buffer_read_begin(NdbTBuffer *buffer);
+extern void		nodedb_t_buffer_read_end(NdbTBuffer *buffer);
 extern char *		nodedb_t_buffer_read_line(NdbTBuffer *buffer, unsigned int line, char *put, size_t putmax);
 extern void		nodedb_t_buffer_insert(NdbTBuffer *buffer, size_t pos, const char *text);
 extern void		nodedb_t_buffer_append(NdbTBuffer *buffer, const char *text);
