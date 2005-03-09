54c184617423dde92ad96448712abde277b4fe68
Emil Brisk <emil.brink@interactiveinstitute.se>
2005-03-09 16:38:20 +0000

    Simplified, Purple does the type-conversion of samples
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@2023 49c17534-1d02-dc11-b283-0030488c597c

--- purple/plugins/audiotone.c
+++ purple/plugins/audiotone.c
@@ -16,14 +16,14 @@
 static PComputeStatus compute(PPInput *input, PPOutput output, void *state)
 {
 	VNALayerType	type = VN_A_LAYER_INT16;
-	real64		sfreq = 44100.0, tone, dur, t, ang;
+	real64		sfreq = 44100.0, tone, dur, t, ang, block[1024];
 	PONode		*out;
 	PNALayer	*layer;
 	unsigned int	i, pos, len, to_go, chunk;
 
 	tone = p_input_real32(input[0]);
 	dur  = p_input_real32(input[1]);
-	/* FIXME: Actually use inputs 2 and 3, here. With defaults, preferably. */
+	/* FIXME: Actually *use* inputs 2 and 3, here. With defaults, preferably. */
 
 	out = p_output_node_create(output, V_NT_AUDIO, 0);
 	layer = p_node_a_layer_create(out, "tone", type, sfreq);
@@ -31,22 +31,16 @@ static PComputeStatus compute(PPInput *input, PPOutput output, void *state)
 	printf("Generating %g seconds of %g Hz sine, sampled at %g Hz\n", dur, tone, sfreq);
 	len = dur * sfreq;
 	printf(" that's %lu samples\n", len);
-	if(type == VN_A_LAYER_INT16)
+	for(pos = 0, to_go = len; to_go > 0; pos += chunk, to_go -= chunk)
 	{
-		int16	block[1024];	/* Generate tone in blocks of 1 ksamples. */
-
-		for(pos = 0, to_go = len; to_go > 0; pos += chunk, to_go -= chunk)
+		chunk = to_go > sizeof block / sizeof *block ? sizeof block / sizeof *block : to_go;
+		for(i = 0; i < chunk; i++)
 		{
-			chunk = to_go > sizeof block / sizeof *block ? sizeof block / sizeof *block : to_go;
-			printf("building chunk %u\n", chunk);
-			for(i = 0; i < chunk; i++)
-			{
-				t   = (pos + i) / sfreq;
-				ang = 2.0 * M_PI * (t * tone);
-				block[i] = pow(2, CHAR_BIT * sizeof *block - 1) * sin(ang);
-			}
-			p_node_a_layer_write_samples(layer, pos, block, chunk);
+			t   = (pos + i) / sfreq;
+			ang = 2.0 * M_PI * (t * tone);
+			block[i] = sin(ang);
 		}
+		p_node_a_layer_write_samples(layer, pos, block, chunk);
 	}
 	return P_COMPUTE_DONE;
 }
