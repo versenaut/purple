736af4c7a3a0cbf2ec69a764f342f64e7a86e22a
Emil Brisk <emil.brink@interactiveinstitute.se>
2005-04-06 07:47:22 +0000

    Disregard deleted vertices
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@2135 49c17534-1d02-dc11-b283-0030488c597c

--- purple/plugins/bbox.c
+++ purple/plugins/bbox.c
@@ -5,6 +5,11 @@
 #include "purple.h"
 #include "purple-plugin.h"
 
+static int vertex_deleted(const real64 *vtx)
+{
+	return vtx != NULL && fabs(*vtx) > 1e300;
+}
+
 /* This gets called whenever the input changes. Traverse vertices and compute bbox. */
 static PComputeStatus compute(PPInput *input, PPOutput output, void *state)
 {
@@ -12,10 +17,17 @@ static PComputeStatus compute(PPInput *input, PPOutput output, void *state)
 	size_t		i, j, k, size;
 	real64		min[4] = { 1E20, 1E20, 1E20, 0.0 }, max[3] = { -1E20, -1E20, -1E20 }, point[3];
 
+	printf("computing bbox\n");
 	for(i = 0; (in = p_input_node_nth(input[0], i)) != NULL; i++)
 	{
 		const PNGLayer *layer;
 
+		/* If object, check its 'geometry' link. */
+		if(p_node_get_type(in) == V_NT_OBJECT)
+		{
+			in = p_node_o_link_get(in, "geometry", 0);
+			printf("geometry link followed, got %p\n", in);
+		}
 		if(p_node_get_type(in) != V_NT_GEOMETRY)
 			continue;
 		layer = p_node_g_layer_find(in, "vertex");
@@ -23,6 +35,8 @@ static PComputeStatus compute(PPInput *input, PPOutput output, void *state)
 		for(j = 0; j < size; j++)
 		{
 			p_node_g_vertex_get_xyz(layer, j, point, point + 1, point + 2);
+			if(vertex_deleted(point))
+				continue;
 			for(k = 0; k < sizeof point / sizeof *point; k++)
 			{
 				if(point[k] > max[k])
@@ -33,6 +47,9 @@ static PComputeStatus compute(PPInput *input, PPOutput output, void *state)
 		}
 		p_output_real64_vec3(output, max);
 		p_output_real64_vec4(output, min);	/* Slightly hackish. */
+		printf("bbox computed: (%g,%g,%g)-(%g,%g,%g)\n",
+		       min[0], min[1], min[2],
+		       max[0], max[1], max[2]);
 		break;		/* Only process first geometry node. */
 	}
 	return P_COMPUTE_DONE;
