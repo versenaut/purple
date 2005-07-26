a65b101bc803c34e1e52588cde6875cb488af858
Emil Brisk <emil.brink@interactiveinstitute.se>
2005-07-26 14:20:49 +0000

    Rewritten, now works
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@2714 49c17534-1d02-dc11-b283-0030488c597c

--- purple/plugins/cone.c
+++ purple/plugins/cone.c
@@ -6,17 +6,18 @@
 
 #include "purple.h"
 
+#define	POLY(l,i,v0,v1,v2,v3)	p_node_g_polygon_set_corner_uint32(l,i,v0,v1,v2,v3)
+
 /* This gets called whenever the input, the size, changes. Create a cube with the given side length. */
 static PComputeStatus compute(PPInput *input, PPOutput output, void *state)
 {
 	enum { MY_OBJECT, MY_GEOMETRY };			/* Node labels. */
 	PONode		*obj, *geo;
-	real32		size = p_input_real32(input[0]);	/* Read out the size. */
-	uint32		splits = p_input_uint32(input[1]);	/* And the tesselation level. */
-	uint32		bottom_splits = p_input_uint32(input[2]);	/* And the tesselation level. */
-	uint32		side_splits = p_input_uint32(input[3]);	/* And the tesselation level. */
-	real64		x, y, f;
-	uint32		i, j, poly, corner;
+	real32		height = p_input_real32(input[0]);	/* Read out the size. */
+	uint32		bottom_splits = p_input_uint32(input[1]);	/* And the tesselation level. */
+	uint32		side_splits = p_input_uint32(input[2]);	/* And the tesselation level. */
+	real64		angle, x, y, r;
+	uint32		i, j, pos, poly, corner, apex, bottom;
 	uint32		v0, v1, v2, v3;
 	PNGLayer	*lay;
 
@@ -25,48 +26,63 @@ static PComputeStatus compute(PPInput *input, PPOutput output, void *state)
 
 	p_node_o_link_set(obj, geo, "geometry", 0);
 
-	/* Create topside polygons. Vertex references will dangle (for a while), but that's OK. */
-	lay = p_node_g_layer_find(geo, "polygon");
-	corner = splits * (bottom_splits + side_splits);
-	poly = 0;
-
-/*	for(i = 0; i < splits; i++)
-		p_node_g_polygon_set_corner_uint32(lay, poly++, corner, i , (i + 1) % splits, -1);
-*/
-	for(i = 0; i < splits * (bottom_splits + side_splits); i++)
-		p_node_g_polygon_set_corner_uint32(lay, poly++, (i + 1) % splits, i, i + splits, (i + 1) % splits + splits);
-
-/*	for(i = 0; i < splits; i++)
-		p_node_g_polygon_set_corner_uint32(lay, poly++, corner + 1, (i + 1) % splits, i, -1);
-*/
+	/* Create bottom vertices. */
 	lay = p_node_g_layer_find(geo, "vertex");
-
-	for(i = 0; i < splits; i++)
+	for(i = 0; i < bottom_splits; i++)
 	{
-		x = sin((double)i / (double)splits * 2 * M_PI);
-		y = cos((double)i / (double)splits * 2 * M_PI);
-		for(j = 0; j < bottom_splits + 1; j++)
+		angle = 2 * M_PI * (i / (double) bottom_splits);
+		p_node_g_vertex_set_xyz(lay, i, cos(angle), 0.0, -sin(angle));
+	}
+
+	/* Create any vertices between the bottom and the top, for along-height splits. */
+	lay = p_node_g_layer_find(geo, "vertex");
+	for(j = 1; j < side_splits; j++)
+	{
+		y = j * height / side_splits;
+		r = 1.0 - ((double) j / side_splits);
+		for(i = 0; i < bottom_splits; i++, pos++)
 		{
-			f = (real64) (j + 1) / (real64)(bottom_splits + 1);
-			p_node_g_vertex_set_xyz(lay, i + j * splits,  x * f, 0, y * f);
-		}
-		for(j = 0; j < side_splits; j++)
-		{
-			f = 1.0 - ((real64)j / (real64)(side_splits + 1));
-			p_node_g_vertex_set_xyz(lay, i + (bottom_splits + 1 + j) * splits, x * f, 1 - f, y * f);
+			angle = 2 * M_PI * (i / (double) bottom_splits);
+			p_node_g_vertex_set_xyz(lay, j * bottom_splits + i, r * cos(angle), y, r * -sin(angle));
 		}
 	}
-	p_node_g_vertex_set_xyz(lay, corner, 0, 0, 0);
-	p_node_g_vertex_set_xyz(lay, corner + 1, 0, 1, 0);
+	/* Create apex vertex. */
+	apex = side_splits * bottom_splits;
+	p_node_g_vertex_set_xyz(lay, apex, 0.0, height, 0.0);
+	/* Create bottom center, "anti-apex". It's at apex+1. */
+	p_node_g_vertex_set_xyz(lay, apex + 1, 0.0, 0.0, 0.0);
+
+	/* Create bottom surface. */
+	lay = p_node_g_layer_find(geo, "polygon");
+	for(i = 0; i < bottom_splits; i++)
+		POLY(lay, i, i, (i + 1) % bottom_splits, apex + 1, ~0u);
+
+	/* Create polygons (quads) between bottom and apex-touching triangles. */
+	for(j = 1; j < side_splits; j++)
+	{
+		pos = (j - 1) * bottom_splits;
+		for(i = 0; i < bottom_splits; i++)
+		{
+			POLY(lay, j * bottom_splits + i,
+			     pos + i + bottom_splits,
+			     pos + (i + 1) % bottom_splits + bottom_splits,
+			     pos + (i + 1) % bottom_splits,
+			     pos + i);
+		}
+	}
+
+	/* Create final row of triangles touching the apex. */
+	pos = apex - bottom_splits;
+	for(i = 0; i < bottom_splits; i++)
+		POLY(lay, bottom_splits + side_splits * bottom_splits + i, pos + (i + 1) % bottom_splits, pos + i, apex, ~0u);
 	return P_COMPUTE_DONE;	/* Sleep until size changes. */
 }
 
 PURPLE_PLUGIN void init(void)
 {
 	p_init_create("cone");
-	p_init_input(0, P_VALUE_REAL32, "size", P_INPUT_REQUIRED, P_INPUT_MIN(0.1), P_INPUT_MAX(200.0), P_INPUT_DEFAULT(10.0), P_INPUT_DONE);
-	p_init_input(1, P_VALUE_UINT32, "splits", P_INPUT_REQUIRED, P_INPUT_MIN(1), P_INPUT_MAX(100), P_INPUT_DEFAULT(1), P_INPUT_DONE);
-	p_init_input(2, P_VALUE_UINT32, "bottom splits", P_INPUT_REQUIRED, P_INPUT_MIN(1), P_INPUT_MAX(100), P_INPUT_DEFAULT(1), P_INPUT_DONE);
-	p_init_input(3, P_VALUE_UINT32, "side splits", P_INPUT_REQUIRED, P_INPUT_MIN(1), P_INPUT_MAX(100), P_INPUT_DEFAULT(1), P_INPUT_DONE);
+	p_init_input(0, P_VALUE_REAL32, "height",        P_INPUT_REQUIRED, P_INPUT_MIN(0.1), P_INPUT_MAX(200.0), P_INPUT_DEFAULT(10.0), P_INPUT_DONE);
+	p_init_input(1, P_VALUE_UINT32, "bottom splits", P_INPUT_REQUIRED, P_INPUT_MIN(1),   P_INPUT_MAX(128),   P_INPUT_DEFAULT(8), P_INPUT_DONE);
+	p_init_input(2, P_VALUE_UINT32, "side splits",   P_INPUT_REQUIRED, P_INPUT_MIN(1),   P_INPUT_MAX(128),   P_INPUT_DEFAULT(1), P_INPUT_DONE);
 	p_init_compute(compute);
 }
