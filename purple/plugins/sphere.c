3f5a89ae0d727c2af5a09eaad479b3d2d36aee1c
Emil Brisk <emil.brink@interactiveinstitute.se>
2005-07-27 09:27:13 +0000

    A sphere primitive
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@2718 49c17534-1d02-dc11-b283-0030488c597c

---
+++ purple/plugins/sphere.c
@@ -0,0 +1,81 @@
+/*
+ * This plug-in creates a sphere, using inputs to set tesselation and height.
+*/
+
+#include <math.h>
+
+#include "purple.h"
+
+#define	POLY(lab,l,i,v0,v1,v2,v3)	do { /*printf(lab " polygon %u: %u-%u-%u-%u\n", i, v0, v1, v2, v3);*/ p_node_g_polygon_set_corner_uint32(l,i,v0,v1,v2,v3); } while(0)
+
+/* This gets called whenever the input, the size, changes. Create a cube with the given side length. */
+static PComputeStatus compute(PPInput *input, PPOutput output, void *state)
+{
+	enum { MY_OBJECT, MY_GEOMETRY };			/* Node labels. */
+	PONode		*obj, *geo;
+	real32		radius = p_input_real32(input[0]);	/* Read out the radius. */
+	uint32		end_splits = p_input_uint32(input[1]);	/* And the top/bottom tesselation level. */
+	uint32		side_splits = p_input_uint32(input[2]);	/* And the side tesselation level. */
+	real64		angle, y, r;
+	uint32		i, j, pos, bc, tc;
+	PNGLayer	*lay;
+
+	obj = p_output_node_create(output, V_NT_OBJECT, MY_OBJECT);
+	geo = p_output_node_create(output, V_NT_GEOMETRY, MY_GEOMETRY);
+
+	p_node_o_link_set(obj, geo, "geometry", 0);
+
+	/* Create outer vertices. */
+	lay = p_node_g_layer_find(geo, "vertex");
+	for(j = 0; j <= side_splits; j++)
+	{
+		y = radius * sin(-M_PI/2 + j * (M_PI / side_splits));
+		r = radius * cos(-M_PI/2 + j * (M_PI / side_splits));
+		for(i = 0; i < end_splits; i++, pos++)
+		{
+			angle = 2 * M_PI * (i / (double) end_splits);
+			p_node_g_vertex_set_xyz(lay, j * end_splits + i, r * cos(angle), y, r * -sin(angle));
+		}
+	}
+	/* Create bottom center vertex. */
+	bc = (side_splits + 1) * end_splits;
+	p_node_g_vertex_set_xyz(lay, bc, 0.0, 0.0, 0.0);
+	/* Create top center. It's at bc+1. */
+	tc = bc + 1;
+	p_node_g_vertex_set_xyz(lay, tc, 0.0, radius, 0.0);
+	/* Create bottom surface. */
+	lay = p_node_g_layer_find(geo, "polygon");
+	for(i = 0; i < end_splits; i++)
+		POLY("bottom", lay, i, i, (i + 1) % end_splits, bc, ~0u);
+	/* Create side polygons (quads). */
+	for(j = 0; j < side_splits; j++)
+	{
+		pos = j * end_splits;
+		for(i = 0; i < end_splits; i++)
+		{
+			POLY("side", lay, end_splits + j * end_splits + i,
+			     pos + i + end_splits,
+			     pos + (i + 1) % end_splits + end_splits,
+			     pos + (i + 1) % end_splits,
+			     pos + i);
+		}
+	}
+	/* Create top surface. */
+	pos = tc - 1 - end_splits;
+	for(i = 0; i < end_splits; i++)
+		POLY("top", lay, end_splits + side_splits * end_splits + i, pos + (i + 1) % end_splits, pos + i, tc, ~0u);
+
+	p_node_g_crease_set_vertex(geo, NULL, ~0u);
+	p_node_g_crease_set_edge(geo, NULL, ~0u);
+
+	return P_COMPUTE_DONE;	/* Sleep until size changes. */
+}
+
+PURPLE_PLUGIN void init(void)
+{
+	p_init_create("sphere");
+	p_init_input(0, P_VALUE_REAL32, "radius",      P_INPUT_REQUIRED, P_INPUT_MIN(0.1), P_INPUT_MAX(200.0), P_INPUT_DEFAULT(10.0), P_INPUT_DONE);
+	p_init_input(1, P_VALUE_UINT32, "end splits",  P_INPUT_REQUIRED, P_INPUT_MIN(1),   P_INPUT_MAX(128),   P_INPUT_DEFAULT(8), P_INPUT_DONE);
+	p_init_input(2, P_VALUE_UINT32, "side splits", P_INPUT_REQUIRED, P_INPUT_MIN(1),   P_INPUT_MAX(128),   P_INPUT_DEFAULT(1), P_INPUT_DONE);
+	p_init_compute(compute);
+}
