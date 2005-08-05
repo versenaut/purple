c9999ca8d2ccd500283a5223b2e9e1ac42988a6e
Emil Brisk <emil.brink@interactiveinstitute.se>
2005-08-05 13:11:23 +0000

    Verbosity; better inputs and more meta
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@2749 49c17534-1d02-dc11-b283-0030488c597c

--- purple/plugins/bmblend.c
+++ purple/plugins/bmblend.c
@@ -18,10 +18,16 @@ static PComputeStatus compute(PPInput *input, PPOutput output, void *state)
 	in1 = p_input_node(input[0]);
 	in2 = p_input_node(input[1]);
 	if(p_node_get_type(in1) != V_NT_BITMAP || p_node_get_type(in2) != V_NT_BITMAP)
+	{
+		printf(" bad node input %d %d\n", p_node_get_type(in1), p_node_get_type(in2));
 		return P_COMPUTE_DONE;
+	}
 	alpha = p_input_real32(input[2]);
 	if(alpha < 0.0 || alpha > 1.0)
+	{
+		printf(" bad input alpha %g\n", alpha);
 		return P_COMPUTE_DONE;
+	}
 	alphainv = 1.0 - alpha;
 
 	/* Compute output size; only operate on common pixels of inputs. No scaling. */
@@ -58,6 +64,8 @@ static PComputeStatus compute(PPInput *input, PPOutput output, void *state)
 			}
 		}
 	}
+	else
+		printf("blblend: couldn't set up operation (%p %p %p)\n", fb1, fb2, fbout);
 	/* Finalize accesses. */
 	if(fb1 != NULL)
 		p_node_b_layer_read_multi_end(in1, fb1);
@@ -72,8 +80,16 @@ static PComputeStatus compute(PPInput *input, PPOutput output, void *state)
 PURPLE_PLUGIN void init(void)
 {
 	p_init_create("bmblend");
-	p_init_input(0, P_VALUE_MODULE, "bitmap1", P_INPUT_REQUIRED, P_INPUT_DONE);
-	p_init_input(1, P_VALUE_MODULE, "bitmap2", P_INPUT_REQUIRED, P_INPUT_DONE);
-	p_init_input(2, P_VALUE_REAL32, "alpha",   P_INPUT_REQUIRED, P_INPUT_DONE);
+	p_init_input(0, P_VALUE_MODULE, "bitmap1", P_INPUT_REQUIRED, P_INPUT_DESC("The first bitmap is input here."), P_INPUT_DONE);
+	p_init_input(1, P_VALUE_MODULE, "bitmap2", P_INPUT_REQUIRED, P_INPUT_DESC("The second bitmap is input here."), P_INPUT_DONE);
+	p_init_input(2, P_VALUE_REAL32, "alpha",   P_INPUT_REQUIRED, P_INPUT_MIN(0), P_INPUT_MAX(1.0),
+		     P_INPUT_DESC("Controls how much of each bitmap is used in the output. "
+				  "the blending equation is: out = (1 - alpha) * bitmap1 + alpha * bitmap2. "
+				  "So, in other words, the output goes from bitmap1 to bitmap2 as the alpha "
+				  "goes from zero to one."),
+		     P_INPUT_DONE);
+	p_init_meta("authors", "Emil Brink");
+	p_init_meta("desc/purpose", "Computes a straightforward \"alpha blend\" between two input images. Uses a third input, the alpha, to control "
+				    "how much of each is to appear on the output.");
 	p_init_compute(compute);
 }
