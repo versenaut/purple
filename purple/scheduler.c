fbf1ddd3dc9b9ee3542259679fb4dda8ef5e6222
Emil Brisk <emil.brink@interactiveinstitute.se>
2005-08-18 08:32:35 +0000

    Include <stdio.h> for printf()
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@2804 49c17534-1d02-dc11-b283-0030488c597c

--- purple/scheduler.c
+++ purple/scheduler.c
@@ -6,6 +6,7 @@
  * A scheduler. Sounds a lot more sophisticated than it really is, at this point.
 */
 
+#include <stdio.h>
 #include <stdlib.h>
 
 #include "purple.h"
@@ -51,7 +52,10 @@ void sched_add(PInstance *inst)
 	Task	*t;
 
 	if(list_find_custom(sched_info.ready, inst, cmp_task_by_inst) != NULL)
+	{
+		printf("add aborted, already in list\n");
 		return;
+	}
 	if(sched_info.chunk_task == NULL)
 		sched_info.chunk_task = memchunk_new("Task", sizeof (Task), 16);
 	if((t = memchunk_alloc(sched_info.chunk_task)) == NULL)
@@ -59,7 +63,7 @@ void sched_add(PInstance *inst)
 	t->inst  = inst;
 	t->count = 0;
 	sched_info.ready = list_append(sched_info.ready, t);
-/*	LOG_MSG(("Added %s to ready-list, there are now %u ready tasks", plugin_name(inst->plugin), list_length(sched_info.ready)));*/
+	LOG_MSG(("Added %s to ready-list, there are now %u ready tasks", plugin_name(inst->plugin), list_length(sched_info.ready)));
 	sched_info.ready_iter = NULL;
 }
 
@@ -69,6 +73,7 @@ void sched_update(void)
 {
 	TimeVal	t;
 	List	*iter, *next;
+	TimeVal	t1;
 
 	timeval_now(&t);
 	iter = sched_info.ready_iter;
@@ -86,7 +91,9 @@ void sched_update(void)
 		if(task->count == 0)			/* First time we run it, make it prepare. */
 			graph_port_output_begin(task->inst->output);
 		task->count++;
+		timeval_now(&t1);
 		res = plugin_instance_compute(task->inst);
+		printf("Spent %g seconds running compute() of %s\n", timeval_elapsed(&t1, NULL), plugin_name(task->inst->plugin));
 		if(res >= PLUGIN_STOP)
 		{
 			PPOutput	out = task->inst->output;	/* Buffer across free(). */
@@ -94,7 +101,7 @@ void sched_update(void)
 			memchunk_free(sched_info.chunk_task, task);
 			list_destroy(iter);
 			graph_port_output_end(out);		/* Don't notify dependants until done. */
-/*			LOG_MSG(("Task removed, there are now %u ready tasks", list_length(sched_info.ready)));*/
+			LOG_MSG(("Task removed, there are now %u ready tasks", list_length(sched_info.ready)));
 		}
 		iter = next;
 	}
