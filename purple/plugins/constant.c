6712d6f9d620a99761cf080ba92011b4011a63be
Emil Brisk <emil.brink@interactiveinstitute.se>
2005-08-24 11:17:41 +0000

    Simple constant-value-emitting plug-in
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@2891 49c17534-1d02-dc11-b283-0030488c597c

---
+++ purple/plugins/constant.c
@@ -0,0 +1,48 @@
+/*
+ * Super-simple plug-in that just takes in a string, and outputs it as some other type. This
+ * type conversion is not even strictly necessary (Purple can do string->number conversions),
+ * but it improves performance slightly.
+*/
+
+#include "purple.h"
+
+static PComputeStatus compute(PPInput *input, PPOutput output, void *state)
+{
+	const char	*str = p_input_string(input[0]);
+	const uint32	type = p_input_uint32(input[1]);
+
+	switch(type)
+	{
+	case 0:
+		p_output_string(output, str);
+		break;
+	case 1:
+		{
+			real64	t = 0.0;
+
+			sscanf(str, "%lg", &t);
+			p_output_real64(output, t);
+		}
+		break;
+	case 2:
+		{
+			uint32	t = 0;
+
+			sscanf(str, "%lu", &t);
+			p_output_uint32(output, t);
+		}
+		break;
+	}
+	return P_COMPUTE_DONE;
+}
+
+void init(void)
+{
+	p_init_create("constant");
+	p_init_input(0, P_VALUE_STRING, "value", P_INPUT_REQUIRED,
+		     P_INPUT_DESC("String representation of desired value is input here."), P_INPUT_DONE);
+	p_init_input(1, P_VALUE_UINT32, "type", P_INPUT_REQUIRED,
+		     P_INPUT_DESC("This controls what type the output value will have."),
+		     P_INPUT_ENUM("0:String|1:Real64|2:UInt32"), P_INPUT_DONE);
+	p_init_compute(compute);
+}
