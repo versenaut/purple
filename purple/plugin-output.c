032e653d4913237297035944e7de2bdc395e40f6
Emil Brisk <emil.brink@interactiveinstitute.se>
2005-08-25 09:10:56 +0000

    Silence
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@2909 49c17534-1d02-dc11-b283-0030488c597c

--- purple/plugin-output.c
+++ purple/plugin-output.c
@@ -29,7 +29,6 @@ static PComputeStatus compute(PPInput *input, PPOutput output, void *state_typel
 	unsigned int	i;
 	PINode		*node;
 
-	printf("now in plugin-output\n");
 	for(i = 0; (node = p_input_node_nth(input[0], i)) != NULL && i < 100; i++)	/* FIXME: Arbitrary limit, for now. */
 		sync_node_add((PONode *) node);
 	return P_COMPUTE_DONE;
