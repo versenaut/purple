96d6e84986c603c065b03316594dc6c541c3105f
Emil Brisk <emil.brink@interactiveinstitute.se>
2005-09-29 13:13:20 +0000

    Added UV map generation support; changed vertex order
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@3056 49c17534-1d02-dc11-b283-0030488c597c

--- purple/plugins/plane.c
+++ purple/plugins/plane.c
@@ -11,6 +11,8 @@ static PComputeStatus compute(PPInput *input, PPOutput output, void *state)
 	PONode		*obj, *geo;
 	real32		size = p_input_real32(input[0]), xp, yp;
 	uint32		splits = p_input_uint32(input[1]), x, y, i, n;
+	boolean		uv_map = p_input_boolean(input[2]);
+	PNGLayer	*ulay, *vlay;
 	PNGLayer	*lay;
 
 	obj = p_output_node_create(output, V_NT_OBJECT, MY_OBJECT);
@@ -19,6 +21,15 @@ static PComputeStatus compute(PPInput *input, PPOutput output, void *state)
 	p_node_set_name(geo, "geo-plane");
 
 	p_node_o_link_set(obj, geo, "geometry", 0);
+
+	if(uv_map)
+	{
+		ulay = p_node_g_layer_create(geo, "map_u", VN_G_LAYER_POLYGON_CORNER_REAL, 0, 0.0);
+		vlay = p_node_g_layer_create(geo, "map_v", VN_G_LAYER_POLYGON_CORNER_REAL, 0, 0.0);
+	}
+	else
+		ulay = vlay = NULL;
+
 	/* (Re)Create the polygons. Vertex references will dangle (for a while), but that's OK. */
 	lay = p_node_g_layer_find(geo, "polygon");
 	for(y = 0, i = 0; y < splits; y++)
@@ -28,17 +39,25 @@ static PComputeStatus compute(PPInput *input, PPOutput output, void *state)
 			n = y * (splits + 1) + x;
 			p_node_g_polygon_set_corner_uint32(lay, i,
 							   n,
-							   n + (splits + 1),
+							   n + 1,
 							   n + (splits + 1) + 1,
-							   n + 1
-							   );
+							   n + (splits + 1));
+			if(uv_map)
+			{
+				p_node_g_polygon_set_corner_real64(ulay, i, (real64) x / splits, (real64) (x + 1) / splits, (real64) (x + 1) / splits, (real64) x / splits);
+				p_node_g_polygon_set_corner_real64(vlay, i,
+								   1.0 - y / (real64) splits,
+								   1.0 - y / (real64) splits,
+								   1.0 - (y + 1) / (real64) splits,
+								   1.0 - (y + 1) / (real64) splits);
+			}
 		}
 	}
 	/* Create the vertices. */
 	lay = p_node_g_layer_find(geo, "vertex");
 	for(y = i = 0; y <= splits; y++)
 	{
-		yp = -0.5f * size + (splits - y) * (size / splits);
+		yp = -0.5f * size + y * (size / splits);
 		for(x = 0; x <= splits; x++, i++)
 		{
 			xp = -0.5 * size + (x * size) / splits;
@@ -46,7 +65,6 @@ static PComputeStatus compute(PPInput *input, PPOutput output, void *state)
 		}
 	}
 	printf("plane created with %u vertices, %u polygons\n", i, splits * splits);
-
 	return P_COMPUTE_DONE;	/* Sleep until size changes. */
 }
 
@@ -57,6 +75,8 @@ PURPLE_PLUGIN void init(void)
 		     P_INPUT_DESC("Side length of plane."), P_INPUT_DONE);
 	p_init_input(1, P_VALUE_UINT32, "splits", P_INPUT_DEFAULT(1), P_INPUT_DEFAULT(1), P_INPUT_REQUIRED,
 		     P_INPUT_DESC("Number of times to split each side."), P_INPUT_DONE);
+	p_init_input(2, P_VALUE_BOOLEAN, "uv-map", P_INPUT_DEFAULT(0),
+		     P_INPUT_DESC("Controls whether or not UV mapping data is created."), P_INPUT_DONE);
 	p_init_meta("authors", "Emil Brink");
 	p_init_meta("desc/purpose", "Create a simple polygonal plane, consisting of many quadrilaterals.");
 	p_init_compute(compute);
