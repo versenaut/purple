bf3d31a848a8b63e4de878295678ed139ece826f
Emil Brisk <emil.brink@interactiveinstitute.se>
2005-10-25 08:07:53 +0000

    Added vector and matrix support
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@3136 49c17534-1d02-dc11-b283-0030488c597c

--- purple/plugins/constant.c
+++ purple/plugins/constant.c
@@ -4,7 +4,29 @@
  * but it improves performance slightly.
 */
 
+#include <ctype.h>
+
 #include "purple.h"
+
+static int vector_parse(const char *str, real64 *vec, int max)
+{
+	char	*eptr;
+	real64	x;
+	int	i;
+
+	for(i = 0; i < max; i++)
+	{
+		x = strtod(str, &eptr);
+		if(eptr > str)
+			vec[i] = x;
+		else
+			break;
+		str = eptr;
+		while(*str && (isspace(*str) || *str == ',') || *str == '[' || *str == ']')
+			str++;
+	}
+	return i;
+}
 
 static PComputeStatus compute(PPInput *input, PPOutput output, void *state)
 {
@@ -32,6 +54,44 @@ static PComputeStatus compute(PPInput *input, PPOutput output, void *state)
 			p_output_uint32(output, t);
 		}
 		break;
+	case 3:
+		{
+			real64	v[4];
+			int	len = vector_parse(str, v, sizeof v / sizeof *v);
+
+			if(len == 1)
+			{
+				v[1] = v[2] = v[3] = v[0];
+				p_output_real64_vec4(output, v);
+			}
+			else if(len == 2)
+				p_output_real64_vec2(output, v);
+			else if(len == 3)
+				p_output_real64_vec3(output, v);
+			else if(len == 4)
+				p_output_real64_vec4(output, v);
+		}
+		break;
+	case 4:
+		{
+			real64	m[16];
+			int	len = vector_parse(str, m, sizeof m / sizeof *m), i;
+
+			if(len == 0)
+				return P_COMPUTE_DONE;
+			if(len == 1)	/* Copy single value onto main diagonal, clearing the rest. */
+			{
+				for(i = 1; i < 16; i++)
+					m[i] = ((i % 5) == 0) ? m[0] : 0.0;
+			}
+			else		/* Zero the rest. */
+			{
+				for(i = len; i < 16; i++)
+					m[i] = 0.0;
+			}
+			p_output_real64_mat16(output, m);
+		}
+		break;
 	}
 	return P_COMPUTE_DONE;
 }
@@ -43,7 +103,7 @@ PURPLE_PLUGIN void init(void)
 		     P_INPUT_DESC("String representation of desired value is input here."), P_INPUT_DONE);
 	p_init_input(1, P_VALUE_UINT32, "type", P_INPUT_REQUIRED,
 		     P_INPUT_DESC("This controls what type the output value will have."),
-		     P_INPUT_ENUM("0:String|1:Real64|2:UInt32"), P_INPUT_DONE);
+		     P_INPUT_ENUM("0:String|1:Real64|2:UInt32|3:Real64_Vector|4:Real64_Matrix"), P_INPUT_DONE);
 	p_init_meta("authors", "Emil Brink");
 	p_init_meta("desc/purpose", "Output a constant value. The value is created by interpreting an input string according to the 'type' "
 		    "setting, and outputting the result. This is useful when the same value needs to be sent to several other plug-ins' "
