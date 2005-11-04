7a106d8dffde7965bb1bd37d8910cab066ea2f6f
Emil Brisk <emil.brink@interactiveinstitute.se>
2005-11-04 09:07:10 +0000

    Added default element to layers and to layer_create(); added selected() API; added set/get of vertex_real; use idtree
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@3197 49c17534-1d02-dc11-b283-0030488c597c

--- purple/nodedb-g.h
+++ purple/nodedb-g.h
@@ -6,14 +6,27 @@
  * Geometry node database implementation.
 */
 
+typedef struct NodeGeometry	NodeGeometry;
+
 typedef struct
 {
 	VLayerID	id;
 	char		name[16];
 	VNGLayerType	type;
 	DynArr		*data;
-	uint32		def_uint;
+	union {				/* Default element as used by the dynamic array. Simply repeats the def values as needed. */
+	real64		v_xyz[3];
+	uint32		v_uint32;
+	real64		v_real;
+	uint32		p_corner_uint32[4];
+	real64		p_corner_real[4];
+	uint8		p_face_uint8;
+	uint32		p_face_uint32;
+	real64		p_face_real;
+	}		def;
+	uint32		def_uint;		/* Default values as known by the Verse server. */
 	real64		def_real;
+	NodeGeometry	*node;
 } NdbGLayer;
 
 typedef struct NdbGBone	NdbGBone;
@@ -31,16 +44,17 @@ struct NdbGBone
 	boolean		pending;
 };
 
-typedef struct
+struct NodeGeometry
 {
 	PNode	node;
+	uint32	num_vertex, num_polygon;
 	DynArr	*layers;
-	DynArr	*bones;
+	IdTree	*bones;
 	struct {
 	char	layer[16];
 	uint32	def;
 	}	crease_vertex, crease_edge;
-} NodeGeometry;
+};
 
 extern void		nodedb_g_construct(NodeGeometry *n);
 extern void		nodedb_g_copy(NodeGeometry *n, const NodeGeometry *src);
@@ -54,13 +68,20 @@ extern NdbGLayer *	nodedb_g_layer_find(const NodeGeometry *n, const char *name);
 extern size_t		nodedb_g_layer_get_size(const NdbGLayer *layer);
 extern const char *	nodedb_g_layer_get_name(const NdbGLayer *layer);
 
-extern NdbGLayer *	nodedb_g_layer_create(NodeGeometry *node, VLayerID layer_id, const char *name, VNGLayerType type);
+extern NdbGLayer *	nodedb_g_layer_create(NodeGeometry *node, VLayerID layer_id, const char *name, VNGLayerType type, uint32 def_uint, real64 def_real);
 extern void		nodedb_g_layer_destroy(NodeGeometry *node, NdbGLayer *layer);
+
+extern void		nodedb_g_layer_set_default(NdbGLayer *layer, uint32 def_uint, real64 def_real);
+
+extern void		nodedb_g_vertex_set_selected(NodeGeometry *node, uint32 vertex_id, real64 value);
+extern real64		nodedb_g_vertex_get_selected(const NodeGeometry *node, uint32 vertex_id);
 
 extern void		nodedb_g_vertex_set_xyz(NdbGLayer *layer, uint32 vertex_id, real64 x, real64 y, real64 z);
 extern void		nodedb_g_vertex_get_xyz(const NdbGLayer *layer, uint32 vertex_id, real64 *x, real64 *y, real64 *z);
 extern void		nodedb_g_vertex_set_uint32(NdbGLayer *layer, uint32 vertex_id, uint32 value);
 extern uint32		nodedb_g_vertex_get_uint32(const NdbGLayer *layer, uint32 vertex_id);
+extern void		nodedb_g_vertex_set_real(NdbGLayer *layer, uint32 vertex_id, real64 value);
+extern real64		nodedb_g_vertex_get_real(const NdbGLayer *layer, uint32 vertex_id);
 extern void		nodedb_g_polygon_set_corner_uint32(NdbGLayer *layer, uint32 polygon_id,
 							   uint32 v0, uint32 v1, uint32 v2, uint32 v3);
 extern void		nodedb_g_polygon_get_corner_uint32(const NdbGLayer *layer, uint32 polygon_id,
