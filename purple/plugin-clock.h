dce3f1c1fc2c0281e3af93b8f20da70b82562737
Emil Brisk <emil.brink@interactiveinstitute.se>
2007-01-18 13:22:14 +0000

    Added missing prototype
    git-svn-id: https://svn.blender.org/svnroot/verse/trunk@4410 49c17534-1d02-dc11-b283-0030488c597c

---
+++ purple/plugin-clock.h
@@ -0,0 +1,9 @@
+/*
+ * plugin-clock.h
+ * 
+ * Copyright (C) 2007 PDC, KTH. See COPYING for license details.
+ * 
+ * Header for built-in "magical" plug-in "input".
+*/
+
+extern void	plugin_clock_init(void);
